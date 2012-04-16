af2fee9891c04e1d8b53a21c4298f927c7933c89
Mark Murphy <mmurphy@xps15.(none)>
2012-04-16 07:55:50 -0400

    new sample apps for v0.4

---
+++ Fragments/Static/src/com/commonsware/android/sfrag/ContentFragment.java
@@ -0,0 +1,102 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.sfrag;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContentFragment extends Fragment implements
+    View.OnClickListener {
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    View result=inflater.inflate(R.layout.mainfrag, container);
+
+    result.findViewById(R.id.showOther).setOnClickListener(this);
+
+    return(result);
+  }
+
+  @Override
+  public void onClick(View v) {
+    ((StaticFragmentsDemoActivity)getActivity()).showOther(v);
+  }
+  
+  @Override
+  public void onAttach(Activity a) {
+    super.onAttach(a);
+    Log.d(getClass().getSimpleName(), "onAttach()");
+  }
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.d(getClass().getSimpleName(), "onCreate()");
+  }
+  
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    Log.d(getClass().getSimpleName(), "onActivityCreated()");
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    Log.d(getClass().getSimpleName(), "onStart()");
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Log.d(getClass().getSimpleName(), "onResume()");
+  }
+
+  @Override
+  public void onPause() {
+    Log.d(getClass().getSimpleName(), "onPause()");
+    super.onPause();
+  }
+
+  @Override
+  public void onStop() {
+    Log.d(getClass().getSimpleName(), "onStop()");
+    super.onStop();
+  }
+
+  @Override
+  public void onDestroyView() {
+    Log.d(getClass().getSimpleName(), "onDestroyView()");
+    super.onDestroyView();
+  }
+
+  @Override
+  public void onDestroy() {
+    Log.d(getClass().getSimpleName(), "onDestroy()");
+    super.onDestroy();
+  }
+
+  @Override
+  public void onDetach() {
+    Log.d(getClass().getSimpleName(), "onDetach()");
+    super.onDetach();
+  }
+}
---
+++ ViewPager/Fragments/src/com/commonsware/android/pager/EditorFragment.java
@@ -0,0 +1,49 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.pager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class EditorFragment extends SherlockFragment {
+  private static final String KEY_POSITION="position";
+
+  static EditorFragment newInstance(int position) {
+    EditorFragment frag=new EditorFragment();
+    Bundle args=new Bundle();
+
+    args.putInt(KEY_POSITION, position);
+    frag.setArguments(args);
+
+    return(frag);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    View result=inflater.inflate(R.layout.editor, container, false);
+    EditText editor=(EditText)result.findViewById(R.id.editor);
+    int position=getArguments().getInt(KEY_POSITION, -1);
+
+    editor.setHint(String.format(getString(R.string.hint), position + 1));
+
+    return(result);
+  }
+}

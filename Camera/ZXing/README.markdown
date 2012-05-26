eb7bdfdd3bc9af44f8f3d4828683ffef8ff0ed9c
Mark Murphy <mmurphy@xps15.(none)>
2012-05-26 09:37:41 -0400

    new sample, using Barcode Scanner

---
+++ Camera/ZXing/README.markdown
@@ -0,0 +1,16 @@
+ZXing Android Integration Demo
+==============================
+This is just a quick-and-dirty sample application showing
+how to use the [ZXing](http://code.google.com/p/zxing/) `IntentIntegrator` class.
+
+License
+-------
+The code in this project is licensed under the Apache
+Software License 2.0, per the terms of the included LICENSE
+file.
+
+Who Made This?
+--------------
+<a href="http://commonsware.com">![CommonsWare](http://commonsware.com/images/logo.png)</a>
+
+[gg]: http://groups.google.com/group/cw-android
---
+++ Camera/ZXing/src/com/commonsware/android/zxing/ZXingDemo.java
@@ -0,0 +1,64 @@
+/***
+	Copyright (c) 2008-2010 CommonsWare, LLC
+	
+	Licensed under the Apache License, Version 2.0 (the "License"); you may
+	not use this file except in compliance with the License. You may obtain
+	a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package com.commonsware.android.zxing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ZXingDemo extends Activity {
+	TextView format=null;
+	TextView contents=null;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		format=(TextView)findViewById(R.id.format);
+		contents=(TextView)findViewById(R.id.contents);
+	}
+	
+	public void doScan(View v) {
+		(new IntentIntegrator(this)).initiateScan();
+	}
+	
+	public void onActivityResult(int request, int result, Intent i) {
+		IntentResult scan=IntentIntegrator.parseActivityResult(request,
+																													 result,
+																													 i);
+		
+		if (scan!=null) {
+			format.setText(scan.getFormatName());
+			contents.setText(scan.getContents());
+		}
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle state) {
+		state.putString("format", format.getText().toString());
+		state.putString("contents", contents.getText().toString());
+	}
+	
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		format.setText(state.getString("format"));
+		contents.setText(state.getString("contents"));
+	}
+}

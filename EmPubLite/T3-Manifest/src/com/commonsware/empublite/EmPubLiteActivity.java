86a6cceaa2295abdecc6048b90328b38ee949bd0
Mark Murphy <mmurphy@commonsware.com>
2012-07-26 09:43:27 -0400

    remove duplicate android-support-v4.jar

--- EmPubLite/T3-Manifest/src/com/commonsware/empublite/EmPubLiteActivity.java
+++ EmPubLite/T3-Manifest/src/com/commonsware/empublite/EmPubLiteActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class EmPubLiteActivity extends Activity {
 
--- EmPubLite/T5-Progress/src/com/commonsware/empublite/EmPubLiteActivity.java
+++ EmPubLite/T5-Progress/src/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,10 +1,8 @@
 package com.commonsware.empublite;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class EmPubLiteActivity extends Activity {
 

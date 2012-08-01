9dbbbb9541df5f14474464935f77568c2fa6b4ad
Mark Murphy <mmurphy@commonsware.com>
2012-08-01 10:06:10 -0400

    rolled back malfunctioning optimization

--- Internet/Weather/src/com/commonsware/android/weather/WebViewFragment.java
+++ Internet/Weather/src/com/commonsware/android/weather/WebViewFragment.java
@@ -16,7 +16,6 @@
  */
 
 //package android.webkit;
-
 package com.commonsware.android.weather;
 
 import android.annotation.TargetApi;
@@ -31,88 +30,89 @@ import com.actionbarsherlock.app.SherlockFragment;
 /**
  * A fragment that displays a WebView.
  * <p>
- * The WebView is automatically paused or resumed when the Fragment is paused or resumed.
+ * The WebView is automatically paused or resumed when the
+ * Fragment is paused or resumed.
  */
 public class WebViewFragment extends SherlockFragment {
-    private WebView mWebView;
-    private boolean mIsWebViewAvailable;
+  private WebView mWebView;
+  private boolean mIsWebViewAvailable;
 
-    public WebViewFragment() {
+  public WebViewFragment() {
+  }
+
+  /**
+   * Called to instantiate the view. Creates and returns the
+   * WebView.
+   */
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    if (mWebView != null) {
+      mWebView.destroy();
+    }
+    
+    mWebView=new WebView(getActivity());
+    mIsWebViewAvailable=true;
+    return mWebView;
+  }
+
+  /**
+   * Called when the fragment is visible to the user and
+   * actively running. Resumes the WebView.
+   */
+  @TargetApi(11)
+  @Override
+  public void onPause() {
+    super.onPause();
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+      mWebView.onPause();
+    }
+  }
+
+  /**
+   * Called when the fragment is no longer resumed. Pauses
+   * the WebView.
+   */
+  @TargetApi(11)
+  @Override
+  public void onResume() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+      mWebView.onResume();
     }
 
-    /**
-     * Called to instantiate the view. Creates and returns the WebView.
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        if (mWebView != null) {
-            // mWebView.destroy();
-          
-          // MLM modification to actually maintain context
-          // when used with setRetainInstance(true)
-          ((ViewGroup)mWebView.getParent()).removeView(mWebView);
-        }
-        else {
-          mWebView = new WebView(getActivity());
-        }
-        
-        mIsWebViewAvailable = true;
-        return mWebView;
-    }
+    super.onResume();
+  }
 
-    /**
-     * Called when the fragment is visible to the user and actively running. Resumes the WebView.
-     */
-    @TargetApi(11)
-    @Override
-    public void onPause() {
-        super.onPause();
-        
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.HONEYCOMB) {
-          mWebView.onPause();
-        }
-    }
+  /**
+   * Called when the WebView has been detached from the
+   * fragment. The WebView is no longer available after this
+   * time.
+   */
+  @Override
+  public void onDestroyView() {
+    mIsWebViewAvailable=false;
+    super.onDestroyView();
+  }
 
-    /**
-     * Called when the fragment is no longer resumed. Pauses the WebView.
-     */
-    @TargetApi(11)
-    @Override
-    public void onResume() {
-      if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.HONEYCOMB) {
-        mWebView.onResume();
-      }
-      
-        super.onResume();
+  /**
+   * Called when the fragment is no longer in use. Destroys
+   * the internal state of the WebView.
+   */
+  @Override
+  public void onDestroy() {
+    if (mWebView != null) {
+      mWebView.destroy();
+      mWebView=null;
     }
+    super.onDestroy();
+  }
 
-    /**
-     * Called when the WebView has been detached from the fragment.
-     * The WebView is no longer available after this time.
-     */
-    @Override
-    public void onDestroyView() {
-        mIsWebViewAvailable = false;
-        super.onDestroyView();
-    }
-
-    /**
-     * Called when the fragment is no longer in use. Destroys the internal state of the WebView.
-     */
-    @Override
-    public void onDestroy() {
-        if (mWebView != null) {
-            mWebView.destroy();
-            mWebView = null;
-        }
-        super.onDestroy();
-    }
-
-    /**
-     * Gets the WebView.
-     */
-    public WebView getWebView() {
-        return mIsWebViewAvailable ? mWebView : null;
-    }
+  /**
+   * Gets the WebView.
+   */
+  public WebView getWebView() {
+    return mIsWebViewAvailable ? mWebView : null;
+  }
 }

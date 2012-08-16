baaba368d79fbb667e1c42986229b9ec6965bc9f
Mark Murphy <mmurphy@commonsware.com>
2012-08-16 14:06:15 -0400

    added copyright/license comment block

--- Push/GCMClient/src/com/commonsware/android/gcm/client/MainActivity.java
+++ Push/GCMClient/src/com/commonsware/android/gcm/client/MainActivity.java
@@ -1,3 +1,17 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+*/
+
 package com.commonsware.android.gcm.client;
 
 import android.app.Activity;
@@ -14,12 +28,12 @@ public class MainActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    GCMRegistrar.checkDevice(this);
+    GCMRegistrar.checkManifest(this);
   }
 
   public void onClick(View v) {
-    GCMRegistrar.checkDevice(this);
-    GCMRegistrar.checkManifest(this);
-
     final String regId=GCMRegistrar.getRegistrationId(this);
 
     if (regId.length() == 0) {

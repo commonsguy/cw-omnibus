efe12b1499baa99fc81c0b976ae745abac4e1358
Mark Murphy <mmurphy@commonsware.com>
2012-08-27 09:44:38 -0400

    sample app for ExpandableListView

---
+++ WidgetCatalog/ExpandableListView/src/com/commonsware/android/wc/elv/JSONExpandableListAdapter.java
@@ -0,0 +1,144 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.wc.elv;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+import java.util.Iterator;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONExpandableListAdapter extends
+    BaseExpandableListAdapter {
+  LayoutInflater inflater=null;
+  JSONObject model=null;
+
+  JSONExpandableListAdapter(LayoutInflater inflater, JSONObject model) {
+    this.inflater=inflater;
+    this.model=model;
+  }
+
+  @Override
+  public int getGroupCount() {
+    return(model.length());
+  }
+
+  @Override
+  public Object getGroup(int groupPosition) {
+    @SuppressWarnings("rawtypes")
+    Iterator i=model.keys();
+
+    while (groupPosition > 0) {
+      i.next();
+      groupPosition--;
+    }
+
+    return(i.next());
+  }
+
+  @Override
+  public long getGroupId(int groupPosition) {
+    return(groupPosition);
+  }
+
+  @Override
+  public View getGroupView(int groupPosition, boolean isExpanded,
+                           View convertView, ViewGroup parent) {
+    if (convertView == null) {
+      convertView=
+          inflater.inflate(android.R.layout.simple_expandable_list_item_1,
+                           parent, false);
+    }
+
+    TextView tv=
+        ((TextView)convertView.findViewById(android.R.id.text1));
+    tv.setText(getGroup(groupPosition).toString());
+
+    return(convertView);
+  }
+
+  @Override
+  public int getChildrenCount(int groupPosition) {
+    try {
+      JSONArray children=getChildren(groupPosition);
+
+      return(children.length());
+    }
+    catch (JSONException e) {
+      // JSONArray is really annoying
+      Log.e(getClass().getSimpleName(), "Exception getting children", e);
+    }
+
+    return(0);
+  }
+
+  @Override
+  public Object getChild(int groupPosition, int childPosition) {
+    try {
+      JSONArray children=getChildren(groupPosition);
+
+      return(children.get(childPosition));
+    }
+    catch (JSONException e) {
+      // JSONArray is really annoying
+      Log.e(getClass().getSimpleName(),
+            "Exception getting item from JSON array", e);
+    }
+
+    return(null);
+  }
+
+  @Override
+  public long getChildId(int groupPosition, int childPosition) {
+    return(groupPosition * 1024 + childPosition);
+  }
+
+  @Override
+  public View getChildView(int groupPosition, int childPosition,
+                           boolean isLastChild, View convertView,
+                           ViewGroup parent) {
+    if (convertView == null) {
+      convertView=
+          inflater.inflate(android.R.layout.simple_list_item_1, parent,
+                           false);
+    }
+
+    TextView tv=(TextView)convertView;
+    tv.setText(getChild(groupPosition, childPosition).toString());
+
+    return(convertView);
+  }
+
+  @Override
+  public boolean isChildSelectable(int groupPosition, int childPosition) {
+    return(true);
+  }
+
+  @Override
+  public boolean hasStableIds() {
+    return(true);
+  }
+
+  private JSONArray getChildren(int groupPosition) throws JSONException {
+    String key=getGroup(groupPosition).toString();
+
+    return(model.getJSONArray(key));
+  }
+}
---
+++ WidgetCatalog/ExpandableListView/src/com/commonsware/android/wc/elv/MainActivity.java
@@ -0,0 +1,108 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.wc.elv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity implements
+    OnChildClickListener, OnGroupClickListener, OnGroupExpandListener,
+    OnGroupCollapseListener {
+  private ExpandableListAdapter adapter=null;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    InputStream raw=getResources().openRawResource(R.raw.sample);
+    BufferedReader in=new BufferedReader(new InputStreamReader(raw));
+    String str;
+    StringBuffer buf=new StringBuffer();
+
+    try {
+      while ((str=in.readLine()) != null) {
+        buf.append(str);
+        buf.append('\n');
+      }
+
+      in.close();
+
+      JSONObject model=new JSONObject(buf.toString());
+
+      ExpandableListView elv=(ExpandableListView)findViewById(R.id.elv);
+
+      adapter=new JSONExpandableListAdapter(getLayoutInflater(), model);
+      elv.setAdapter(adapter);
+
+      elv.setOnChildClickListener(this);
+      elv.setOnGroupClickListener(this);
+      elv.setOnGroupExpandListener(this);
+      elv.setOnGroupCollapseListener(this);
+    }
+    catch (Exception e) {
+      Log.e(getClass().getName(), "Exception reading JSON", e);
+    }
+  }
+
+  @Override
+  public boolean onChildClick(ExpandableListView parent, View v,
+                              int groupPosition, int childPosition,
+                              long id) {
+    Toast.makeText(this,
+                   adapter.getChild(groupPosition, childPosition)
+                          .toString(), Toast.LENGTH_SHORT).show();
+
+    return(false);
+  }
+
+  @Override
+  public boolean onGroupClick(ExpandableListView parent, View v,
+                              int groupPosition, long id) {
+    Toast.makeText(this, adapter.getGroup(groupPosition).toString(),
+                   Toast.LENGTH_SHORT).show();
+
+    return(false);
+  }
+
+  @Override
+  public void onGroupExpand(int groupPosition) {
+    Toast.makeText(this,
+                   "Expanding: "
+                       + adapter.getGroup(groupPosition).toString(),
+                   Toast.LENGTH_SHORT).show();
+  }
+
+  @Override
+  public void onGroupCollapse(int groupPosition) {
+    Toast.makeText(this,
+                   "Collapsing: "
+                       + adapter.getGroup(groupPosition).toString(),
+                   Toast.LENGTH_SHORT).show();
+  }
+}

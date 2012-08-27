e7b3302de7affa444bd6193c80903028cd66b189
Mark Murphy <mmurphy@commonsware.com>
2012-08-27 09:44:05 -0400

    sample app demonstrating uses of GridLayout

---
+++ GridLayout/Sampler/src/com/commonsware/android/gridlayout/SampleAdapter.java
@@ -0,0 +1,59 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.gridlayout;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+
+public class SampleAdapter extends FragmentPagerAdapter {
+  static ArrayList<Sample> SAMPLES=new ArrayList<Sample>();
+  private Context ctxt=null;
+
+  static {
+    SAMPLES.add(new Sample(R.layout.row, R.string.row));
+    SAMPLES.add(new Sample(R.layout.column, R.string.column));
+    SAMPLES.add(new Sample(R.layout.table, R.string.table));
+    SAMPLES.add(new Sample(R.layout.table_flex, R.string.flexible_table));
+    SAMPLES.add(new Sample(R.layout.implicit, R.string.implicit));
+    SAMPLES.add(new Sample(R.layout.spans, R.string.spans));
+  }
+
+  public SampleAdapter(Context ctxt, FragmentManager mgr) {
+    super(mgr);
+    this.ctxt=ctxt;
+  }
+
+  @Override
+  public int getCount() {
+    return(SAMPLES.size());
+  }
+
+  @Override
+  public Fragment getItem(int position) {
+    return(TrivialFragment.newInstance(getSample(position).layoutId));
+  }
+
+  @Override
+  public String getPageTitle(int position) {
+    return(ctxt.getString(getSample(position).titleId));
+  }
+
+  private Sample getSample(int position) {
+    return(SAMPLES.get(position));
+  }
+}

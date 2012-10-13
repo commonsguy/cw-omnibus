1693760a17c7717ace272aabdd7ea708f6f10605
Mark Murphy <mmurphy@commonsware.com>
2012-10-13 17:33:31 -0400

    new progress indication samples

---
+++ Progress/Dialog/src/com/commonsware/android/progdlg/SampleDialogFragment.java
@@ -0,0 +1,51 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.progdlg;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SampleDialogFragment extends DialogFragment {
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    ProgressDialog dlg=new ProgressDialog(getActivity());
+
+    dlg.setMessage(getActivity().getString(R.string.dlg_title));
+    dlg.setIndeterminate(true);
+    dlg.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+    return(dlg);
+  }
+
+  @Override
+  public void onDismiss(DialogInterface unused) {
+    super.onDismiss(unused);
+
+    Log.d(getClass().getSimpleName(), "Goodbye!");
+  }
+
+  @Override
+  public void onCancel(DialogInterface unused) {
+    super.onCancel(unused);
+
+    Toast.makeText(getActivity(), R.string.back, Toast.LENGTH_LONG)
+         .show();
+  }
+}
---
+++ Progress/Styled/src/com/commonsware/android/progress/MainActivity.java
@@ -0,0 +1,67 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+*/
+
+package com.commonsware.android.progress;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends Activity implements Runnable {
+  private static final int PERIOD_SECONDS=1;
+  private ScheduledThreadPoolExecutor executor=
+      new ScheduledThreadPoolExecutor(1);
+  private ProgressBar primary=null;
+  private ProgressBar secondary=null;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    primary=(ProgressBar)findViewById(R.id.progressHS);
+    secondary=(ProgressBar)findViewById(R.id.progressHS2);
+
+    executor.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+    executor.scheduleAtFixedRate(this, 0, PERIOD_SECONDS,
+                                 TimeUnit.SECONDS);
+  }
+
+  @Override
+  public void onDestroy() {
+    executor.shutdown();
+
+    super.onDestroy();
+  }
+
+  @Override
+  public void run() {
+    if (primary.getProgress() < 100) {
+      primary.incrementProgressBy(2);
+      secondary.incrementProgressBy(2);
+
+      if (secondary.getSecondaryProgress() == 100) {
+        secondary.setSecondaryProgress(10);
+      }
+      else {
+        secondary.incrementSecondaryProgressBy(10);
+      }
+    }
+    else {
+      executor.remove(this);
+    }
+  }
+}

45a3b9b1db0a61619abefdc944b3312748d92d5b
Mark Murphy <mmurphy@commonsware.com>
2012-11-05 08:06:57 -0500

    StackView sample

---
+++ WidgetCatalog/StackView/src/com/commonsware/android/wc/stack/MainActivity.java
@@ -0,0 +1,57 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.wc.stack;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.StackView;
+
+public class MainActivity extends Activity {
+  static String[] items= { "lorem", "ipsum", "dolor", "sit", "amet",
+      "consectetuer", "adipiscing", "elit", "morbi", "vel", "ligula",
+      "vitae", "arcu", "aliquet", "mollis", "etiam", "vel", "erat",
+      "placerat", "ante", "porttitor", "sodales", "pellentesque",
+      "augue", "purus" };
+  StackView stack;
+
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    setContentView(R.layout.main);
+
+    stack=(StackView)findViewById(R.id.details);
+    stack.setAdapter(new ItemAdapter(this, R.layout.item, items));
+  }
+
+  private static class ItemAdapter extends ArrayAdapter<String> {
+    public ItemAdapter(Context context, int textViewResourceId,
+                       String[] objects) {
+      super(context, textViewResourceId, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      View result=super.getView(position, convertView, parent);
+
+      result.setBackgroundColor(0xFF330000 + (position * 0x0A0A));
+
+      return(result);
+    }
+  }
+}

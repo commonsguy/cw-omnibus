25c94ca722bf5b6f9fbaf3fb6319eebe2f259b7f
Mark Murphy <mmurphy@commonsware.com>
2012-11-05 08:05:12 -0500

    new XML drawables samples

---
+++ Drawable/ScaleClip/src/com/commonsware/android/scaleclip/FragmentBase.java
@@ -0,0 +1,63 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.scaleclip;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import com.actionbarsherlock.app.SherlockFragment;
+
+abstract public class FragmentBase extends SherlockFragment implements
+    SeekBar.OnSeekBarChangeListener {
+  abstract void setImageBackground(ImageView image);
+  
+  private ImageView image=null;
+
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    setRetainInstance(true);
+
+    View result=inflater.inflate(R.layout.scaleclip, container, false);
+    SeekBar bar=((SeekBar)result.findViewById(R.id.level));
+    
+    bar.setOnSeekBarChangeListener(this);
+    image=(ImageView)result.findViewById(R.id.image);
+    setImageBackground(image);
+    image.setImageLevel(bar.getProgress());
+
+    return(result);
+  }
+  
+  @Override
+  public void onProgressChanged(SeekBar seekBar, int progress,
+                                boolean fromUser) {
+    image.setImageLevel(progress);
+  }
+
+  @Override
+  public void onStartTrackingTouch(SeekBar seekBar) {
+    // no-op
+  }
+
+  @Override
+  public void onStopTrackingTouch(SeekBar seekBar) {
+    // no-op
+  }
+}
---
+++ Drawable/ScaleClip/src/com/commonsware/android/scaleclip/SampleAdapter.java
@@ -0,0 +1,49 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.scaleclip;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SampleAdapter extends FragmentPagerAdapter {
+  static private final int TITLES[]={R.string.scale, R.string.clip};
+  private Context ctxt=null;
+
+  public SampleAdapter(Context ctxt, FragmentManager mgr) {
+    super(mgr);
+    this.ctxt=ctxt;
+  }
+
+  @Override
+  public int getCount() {
+    return(2);
+  }
+
+  @Override
+  public Fragment getItem(int position) {
+    if (position==0) {
+      return(new ScaleFragment());
+    }
+    
+    return(new ClipFragment());
+  }
+
+  @Override
+  public String getPageTitle(int position) {
+    return(ctxt.getString(TITLES[position]));
+  }
+}

58e9289219fa80d8d01b39c83979eeb49d224692
Mark Murphy <mmurphy@commonsware.com>
2012-11-05 08:05:33 -0500

    updated JNI example for x86

---
+++ JNI/WeakBench/obj/local/armeabi-v7a/objs/weakbench/weakbench.o.d
@@ -0,0 +1,85 @@
+obj/local/armeabi-v7a/objs/weakbench/weakbench.o: jni/weakbench.c \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/string.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/alloca.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/strings.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/memory.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/math.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/limits.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/syslimits.h \
+ /opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/jni.h
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/string.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/strings.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/memory.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/math.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
+
+/opt/android-ndk-r7/platforms/android-5/arch-arm/usr/include/jni.h:
---
+++ JNI/WeakBench/obj/local/x86/objs/weakbench/weakbench.o.d
@@ -0,0 +1,88 @@
+obj/local/x86/objs/weakbench/weakbench.o: jni/weakbench.c \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/stdlib.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/string.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/alloca.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/strings.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/memory.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/math.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/limits.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ /opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/jni.h
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/stdlib.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/string.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/strings.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/memory.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/math.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/limits.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+/opt/android-ndk-r7/platforms/android-9/arch-x86/usr/include/jni.h:

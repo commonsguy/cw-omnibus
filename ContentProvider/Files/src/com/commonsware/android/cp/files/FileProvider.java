f62620b9d5bbb67919965c83c03cef02a3a2ec5d
Mark Murphy <mmurphy@commonsware.com>
2012-11-05 08:10:05 -0500

    minor formatting

--- ContentProvider/Files/src/com/commonsware/android/cp/files/FileProvider.java
+++ ContentProvider/Files/src/com/commonsware/android/cp/files/FileProvider.java
@@ -10,9 +10,8 @@
   
   From _The Busy Coder's Guide to Android Development_
     http://commonsware.com/Android
-*/
+ */
 
-   
 package com.commonsware.android.cp.files;
 
 import android.content.ContentProvider;
@@ -30,9 +29,11 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class FileProvider extends ContentProvider {
-  public static final Uri CONTENT_URI=Uri.parse("content://com.commonsware.android.cp.files/");
-  private static final HashMap<String, String> MIME_TYPES=new HashMap<String, String>();
-  
+  public static final Uri CONTENT_URI=
+      Uri.parse("content://com.commonsware.android.cp.files/");
+  private static final HashMap<String, String> MIME_TYPES=
+      new HashMap<String, String>();
+
   static {
     MIME_TYPES.put(".pdf", "application/pdf");
   }
@@ -40,79 +41,80 @@ public class FileProvider extends ContentProvider {
   @Override
   public boolean onCreate() {
     File f=new File(getContext().getFilesDir(), "test.pdf");
-    
+
     if (!f.exists()) {
       AssetManager assets=getContext().getResources().getAssets();
-    
+
       try {
         copy(assets.open("test.pdf"), f);
       }
       catch (IOException e) {
         Log.e("FileProvider", "Exception copying from assets", e);
-        
+
         return(false);
       }
     }
-    
+
     return(true);
   }
 
   @Override
   public String getType(Uri uri) {
     String path=uri.toString();
-    
+
     for (String extension : MIME_TYPES.keySet()) {
       if (path.endsWith(extension)) {
         return(MIME_TYPES.get(extension));
       }
     }
-    
+
     return(null);
   }
-  
+
   @Override
   public ParcelFileDescriptor openFile(Uri uri, String mode)
-    throws FileNotFoundException {
+                                                            throws FileNotFoundException {
     File f=new File(getContext().getFilesDir(), uri.getPath());
-    
+
     if (f.exists()) {
       return(ParcelFileDescriptor.open(f,
-                                        ParcelFileDescriptor.MODE_READ_ONLY));
+                                       ParcelFileDescriptor.MODE_READ_ONLY));
     }
 
-    throw new FileNotFoundException(uri.getPath());  
+    throw new FileNotFoundException(uri.getPath());
   }
-  
+
   @Override
   public Cursor query(Uri url, String[] projection, String selection,
-                        String[] selectionArgs, String sort) {
+                      String[] selectionArgs, String sort) {
     throw new RuntimeException("Operation not supported");
   }
-  
+
   @Override
   public Uri insert(Uri uri, ContentValues initialValues) {
     throw new RuntimeException("Operation not supported");
   }
-  
+
   @Override
-  public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
+  public int update(Uri uri, ContentValues values, String where,
+                    String[] whereArgs) {
     throw new RuntimeException("Operation not supported");
   }
-  
+
   @Override
   public int delete(Uri uri, String where, String[] whereArgs) {
     throw new RuntimeException("Operation not supported");
   }
-  
+
   static private void copy(InputStream in, File dst) throws IOException {
     FileOutputStream out=new FileOutputStream(dst);
     byte[] buf=new byte[1024];
     int len;
-    
-    while((len=in.read(buf))>0) {
+
+    while ((len=in.read(buf)) > 0) {
       out.write(buf, 0, len);
     }
-    
+
     in.close();
     out.close();
   }

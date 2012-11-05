a1d9b0e5661ea416e0e139d30ab052451b18c124
Mark Murphy <mmurphy@commonsware.com>
2012-11-05 08:08:57 -0500

    added copyright/license header and simplified example

--- Threads/PostDelayed/src/com/commonsware/android/post/PostDelayedDemo.java
+++ Threads/PostDelayed/src/com/commonsware/android/post/PostDelayedDemo.java
@@ -1,3 +1,17 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
 package com.commonsware.android.post;
 
 import android.app.Activity;
@@ -5,36 +19,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class PostDelayedDemo extends Activity {
+public class PostDelayedDemo extends Activity implements Runnable {
   private static final int PERIOD=5000;
   private View root=null;
-  
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     root=findViewById(android.R.id.content);
   }
-  
+
   @Override
   public void onResume() {
     super.onResume();
-    
-    everyFiveSeconds.run();
+
+    run();
   }
-  
+
   @Override
   public void onPause() {
-    root.removeCallbacks(everyFiveSeconds);
-    
+    root.removeCallbacks(this);
+
     super.onPause();
   }
 
-  Runnable everyFiveSeconds=new Runnable() {
-    public void run() {
-      Toast.makeText(PostDelayedDemo.this, "Who-hoo!",
-                     Toast.LENGTH_SHORT).show();
-      root.postDelayed(this, PERIOD);
-    }
-  };
+  @Override
+  public void run() {
+    Toast.makeText(PostDelayedDemo.this, "Who-hoo!", Toast.LENGTH_SHORT)
+         .show();
+    root.postDelayed(this, PERIOD);
+  }
 }

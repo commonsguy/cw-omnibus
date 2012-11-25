ddac6d2f8c91fafc70d15f6e5845ac546b00f603
Mark Murphy <mmurphy@commonsware.com>
2012-11-25 16:44:42 -0500

    new SeekBar sample

---
+++ WidgetCatalog/SeekBar/src/com/commonsware/android/wc/seekbar/MainActivity.java
@@ -0,0 +1,54 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.wc.seekbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements
+    OnSeekBarChangeListener {
+  TextView value=null;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    value=(TextView)findViewById(R.id.value);
+
+    SeekBar seekBar=(SeekBar)findViewById(R.id.seek_bar);
+
+    seekBar.setOnSeekBarChangeListener(this);
+  }
+
+  @Override
+  public void onProgressChanged(SeekBar seekBar, int progress,
+                                boolean fromUser) {
+    value.setText(String.valueOf(progress));
+  }
+
+  @Override
+  public void onStartTrackingTouch(SeekBar seekBar) {
+    // no-op
+  }
+
+  @Override
+  public void onStopTrackingTouch(SeekBar seekBar) {
+    // no-op
+  }
+}

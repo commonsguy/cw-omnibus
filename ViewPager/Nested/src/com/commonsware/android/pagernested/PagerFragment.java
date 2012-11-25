d2346b38e854f09776175720f15bc32513b37cd5
Mark Murphy <mmurphy@commonsware.com>
2012-11-25 16:49:15 -0500

    new sample using nested fragments

---
+++ ViewPager/Nested/src/com/commonsware/android/pagernested/PagerFragment.java
@@ -0,0 +1,41 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.pagernested;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PagerFragment extends Fragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    View result=inflater.inflate(R.layout.pager, container, false);
+    ViewPager pager=(ViewPager)result.findViewById(R.id.pager);
+
+    pager.setAdapter(buildAdapter());
+
+    return(result);
+  }
+
+  private PagerAdapter buildAdapter() {
+    return(new SampleAdapter(getActivity(), getChildFragmentManager()));
+  }
+}

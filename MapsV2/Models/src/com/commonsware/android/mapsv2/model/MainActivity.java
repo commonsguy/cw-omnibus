e943299a42f2d18fcb9bd2ffb52a8fee55e945b9
Mark Murphy <mmurphy@commonsware.com>
2013-02-19 08:20:02 -0500

    sample of custom models for Maps V2

---
+++ MapsV2/Models/src/com/commonsware/android/mapsv2/model/MainActivity.java
@@ -0,0 +1,170 @@
+/***
+  Copyright (c) 2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.mapsv2.model;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AbstractMapActivity implements
+    OnNavigationListener, OnInfoWindowClickListener {
+  private static final String STATE_NAV="nav";
+  private static final int[] MAP_TYPE_NAMES= { R.string.normal,
+      R.string.hybrid, R.string.satellite, R.string.terrain };
+  private static final int[] MAP_TYPES= { GoogleMap.MAP_TYPE_NORMAL,
+      GoogleMap.MAP_TYPE_HYBRID, GoogleMap.MAP_TYPE_SATELLITE,
+      GoogleMap.MAP_TYPE_TERRAIN };
+  private HashMap<String, Model> models=new HashMap<String, Model>();
+  private GoogleMap map=null;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Model model=
+        new Model(this, String.valueOf(R.string.un),
+                  40.748963847316034, -73.96807193756104, R.string.un,
+                  R.string.united_nations);
+
+    models.put(model.getKey(), model);
+
+    model=
+        new Model(this, String.valueOf(R.string.lincoln_center),
+                  40.76866299974387, -73.98268461227417,
+                  R.string.lincoln_center,
+                  R.string.lincoln_center_snippet);
+    models.put(model.getKey(), model);
+
+    model=
+        new Model(this, String.valueOf(R.string.carnegie_hall),
+                  40.765136435316755, -73.97989511489868,
+                  R.string.carnegie_hall, R.string.practice_x3);
+    models.put(model.getKey(), model);
+
+    model=
+        new Model(this, String.valueOf(R.string.downtown_club),
+                  40.70686417491799, -74.01572942733765,
+                  R.string.downtown_club, R.string.heisman_trophy);
+    models.put(model.getKey(), model);
+
+    if (readyToGo()) {
+      setContentView(R.layout.activity_main);
+
+      SupportMapFragment mapFrag=
+          (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+
+      initListNav();
+
+      map=mapFrag.getMap();
+
+      if (savedInstanceState == null) {
+        CameraUpdate center=
+            CameraUpdateFactory.newLatLng(new LatLng(40.76793169992044,
+                                                     -73.98180484771729));
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+
+        map.moveCamera(center);
+        map.animateCamera(zoom);
+      }
+
+      addMarkers(map);
+
+      map.setInfoWindowAdapter(new PopupAdapter(getLayoutInflater(),
+                                                models));
+      map.setOnInfoWindowClickListener(this);
+    }
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+    map.setMapType(MAP_TYPES[itemPosition]);
+
+    return(true);
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+
+    savedInstanceState.putInt(STATE_NAV,
+                              getSupportActionBar().getSelectedNavigationIndex());
+  }
+
+  @Override
+  public void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+
+    getSupportActionBar().setSelectedNavigationItem(savedInstanceState.getInt(STATE_NAV));
+  }
+
+  @Override
+  public void onInfoWindowClick(Marker marker) {
+    Toast.makeText(this, marker.getTitle(), Toast.LENGTH_LONG).show();
+  }
+
+  private void initListNav() {
+    ArrayList<String> items=new ArrayList<String>();
+    ArrayAdapter<String> nav=null;
+    ActionBar bar=getSupportActionBar();
+
+    for (int type : MAP_TYPE_NAMES) {
+      items.add(getString(type));
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+      nav=
+          new ArrayAdapter<String>(
+                                   bar.getThemedContext(),
+                                   android.R.layout.simple_spinner_item,
+                                   items);
+    }
+    else {
+      nav=
+          new ArrayAdapter<String>(
+                                   this,
+                                   android.R.layout.simple_spinner_item,
+                                   items);
+    }
+
+    nav.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    bar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+    bar.setListNavigationCallbacks(nav, this);
+  }
+
+  private void addMarkers(GoogleMap map) {
+    for (Model model : models.values()) {
+      LatLng position=
+          new LatLng(model.getLatitude(), model.getLongitude());
+
+      map.addMarker(new MarkerOptions().position(position)
+                                       .title(model.getTitle())
+                                       .snippet(model.getKey()));
+
+    }
+  }
+}
---
+++ MapsV2/Models/src/com/commonsware/android/mapsv2/model/Model.java
@@ -0,0 +1,54 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.mapsv2.model;
+
+import android.content.Context;
+
+public class Model {
+  String key;
+  String title;
+  String snippet;
+  double lat;
+  double lon;
+
+  Model(Context ctxt, String key, double lat, double lon, int title,
+        int snippet) {
+    this.key=key;
+    this.title=ctxt.getString(title);
+    this.snippet=ctxt.getString(snippet);
+    this.lat=lat;
+    this.lon=lon;
+  }
+
+  String getKey() {
+    return(key);
+  }
+
+  String getTitle() {
+    return(title);
+  }
+
+  String getSnippet() {
+    return(snippet);
+  }
+
+  double getLatitude() {
+    return(lat);
+  }
+
+  double getLongitude() {
+    return(lon);
+  }
+}

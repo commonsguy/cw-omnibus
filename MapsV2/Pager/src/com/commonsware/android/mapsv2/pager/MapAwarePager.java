6a8b47476c19a65242b958cbc6d0faaac22f9644
Mark Murphy <mmurphy@commonsware.com>
2013-03-19 11:51:03 -0400

    support for scrollable contents

---
+++ MapsV2/Pager/src/com/commonsware/android/mapsv2/pager/MapAwarePager.java
@@ -0,0 +1,38 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.mapsv2.pager;
+
+import android.content.Context;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+import android.view.View;
+
+public class MapAwarePager extends ViewPager {
+  public MapAwarePager(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  protected boolean canScroll(View v, boolean checkV, int dx, int x,
+                              int y) {
+    if (v instanceof SurfaceView || v instanceof PagerTabStrip) {
+      return(true);
+    }
+    
+    return(super.canScroll(v, checkV, dx, x, y));
+  }
+}

00b69a04339d5f9a0bf6e6b99743bc074b83fccb
Mark Murphy <mmurphy@commonsware.com>
2013-03-19 11:54:05 -0400

    sample use of CalendarView

---
+++ WidgetCatalog/CalendarView/src/com/commonsware/android/wc/calendar/CalendarDemoActivity.java
@@ -0,0 +1,46 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.wc.calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalendarDemoActivity extends Activity implements
+    OnDateChangeListener {
+  CalendarView calendar=null;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    calendar=(CalendarView)findViewById(R.id.calendar);
+    calendar.setOnDateChangeListener(this);
+  }
+
+  @Override
+  public void onSelectedDayChange(CalendarView view, int year,
+                                  int monthOfYear, int dayOfMonth) {
+    Calendar then=new GregorianCalendar(year, monthOfYear, dayOfMonth);
+
+    Toast.makeText(this, then.getTime().toString(), Toast.LENGTH_LONG)
+         .show();
+  }
+}

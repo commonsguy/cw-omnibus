b14b025c3943f63aaa9efd7a6952286afea34a32
Mark Murphy <mmurphy@commonsware.com>
2013-05-03 10:01:12 -0400

    sample custom widget

---
+++ Views/ReverseChronometer/src/com/commonsware/android/revchron/MainActivity.java
@@ -0,0 +1,46 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.revchron;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+  private ReverseChronometer chrono=null;
+      
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    
+    chrono=(ReverseChronometer)findViewById(R.id.chrono);
+    chrono.setOverallDuration(90);
+    chrono.setWarningDuration(10);
+  }
+  
+  @Override
+  public void onResume() {
+    super.onResume();
+    
+    chrono.run();
+  }
+  
+  @Override
+  public void onPause() {
+    chrono.stop();
+    
+    super.onPause();
+  }
+}

a0320a109a887702d3b609c3cac1c3fb7bc50650
Mark Murphy <mmurphy@commonsware.com>
2013-05-03 10:00:29 -0400

    sample using CWAC-Presentation

---
+++ Presentation/Simpler/src/com/commonsware/android/preso/simpler/MainActivity.java
@@ -0,0 +1,80 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.preso.simpler;
+
+import android.app.Activity;
+import android.app.Presentation;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Display;
+import android.webkit.WebView;
+import com.commonsware.cwac.preso.PresentationHelper;
+
+public class MainActivity extends Activity implements
+    PresentationHelper.Listener {
+  Presentation preso=null;
+  PresentationHelper helper=null;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+    helper=new PresentationHelper(this, this);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    helper.onResume();
+  }
+
+  @Override
+  public void onPause() {
+    helper.onPause();
+    super.onPause();
+  }
+
+  @Override
+  public void clearPreso(boolean showInline) {
+    if (preso != null) {
+      preso.dismiss();
+      preso=null;
+    }
+  }
+
+  @Override
+  public void showPreso(Display display) {
+    preso=new SimplerPresentation(this, display);
+    preso.show();
+  }
+
+  private class SimplerPresentation extends Presentation {
+    SimplerPresentation(Context ctxt, Display display) {
+      super(ctxt, display);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      WebView wv=new WebView(getContext());
+
+      wv.loadUrl("http://commonsware.com");
+
+      setContentView(wv);
+    }
+  }
+}

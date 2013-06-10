d959557ac868fb2002b8e9d1901cf55da6454ed9
Mark Murphy <mmurphy@commonsware.com>
2013-06-10 08:10:56 -0400

    fixed missing permission; improved OpenGL ES handling

--- MapsV2/NooYawk/src/com/commonsware/android/mapsv2/nooyawk/AbstractMapActivity.java
+++ MapsV2/NooYawk/src/com/commonsware/android/mapsv2/nooyawk/AbstractMapActivity.java
@@ -15,8 +15,11 @@
 package com.commonsware.android.mapsv2.nooyawk;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.FeatureInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.widget.Toast;
@@ -52,7 +55,13 @@ public class AbstractMapActivity extends SherlockFragmentActivity {
         GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
 
     if (status == ConnectionResult.SUCCESS) {
-      return(true);
+      if (getVersionFromPackageManager(this) >= 2) {
+        return(true);
+      }
+      else {
+        Toast.makeText(this, R.string.no_maps, Toast.LENGTH_LONG).show();
+        finish();
+      }
     }
     else if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
       ErrorDialogFragment.newInstance(status)
@@ -63,7 +72,7 @@ public class AbstractMapActivity extends SherlockFragmentActivity {
       Toast.makeText(this, R.string.no_maps, Toast.LENGTH_LONG).show();
       finish();
     }
-    
+
     return(false);
   }
 
@@ -96,5 +105,53 @@ public class AbstractMapActivity extends SherlockFragmentActivity {
         getActivity().finish();
       }
     }
+  }
+
+  // following from
+  // https://android.googlesource.com/platform/cts/+/master/tests/tests/graphics/src/android/opengl/cts/OpenGlEsVersionTest.java
+
+  /*
+   * Copyright (C) 2010 The Android Open Source Project
+   * 
+   * Licensed under the Apache License, Version 2.0 (the
+   * "License"); you may not use this file except in
+   * compliance with the License. You may obtain a copy of
+   * the License at
+   * 
+   * http://www.apache.org/licenses/LICENSE-2.0
+   * 
+   * Unless required by applicable law or agreed to in
+   * writing, software distributed under the License is
+   * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   * CONDITIONS OF ANY KIND, either express or implied. See
+   * the License for the specific language governing
+   * permissions and limitations under the License.
+   */
+
+  private static int getVersionFromPackageManager(Context context) {
+    PackageManager packageManager=context.getPackageManager();
+    FeatureInfo[] featureInfos=
+        packageManager.getSystemAvailableFeatures();
+    if (featureInfos != null && featureInfos.length > 0) {
+      for (FeatureInfo featureInfo : featureInfos) {
+        // Null feature name means this feature is the open
+        // gl es version feature.
+        if (featureInfo.name == null) {
+          if (featureInfo.reqGlEsVersion != FeatureInfo.GL_ES_VERSION_UNDEFINED) {
+            return getMajorVersion(featureInfo.reqGlEsVersion);
+          }
+          else {
+            return 1; // Lack of property means OpenGL ES
+                      // version 1
+          }
+        }
+      }
+    }
+    return 1;
+  }
+
+  /** @see FeatureInfo#getGlEsVersion() */
+  private static int getMajorVersion(int glEsVersion) {
+    return((glEsVersion & 0xffff0000) >> 16);
   }
 }

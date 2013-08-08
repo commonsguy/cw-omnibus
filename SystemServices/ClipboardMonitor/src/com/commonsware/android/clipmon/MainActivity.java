9795881fa115155ebf0868ca97aeae0beed95692
Mark Murphy <mmurphy@commonsware.com>
2013-08-08 13:02:38 -0400

    demonstration of addPrimaryClipChangedListener()

---
+++ SystemServices/ClipboardMonitor/src/com/commonsware/android/clipmon/MainActivity.java
@@ -0,0 +1,49 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+*/
+
+package com.commonsware.android.clipmon;
+
+import android.app.Activity;
+import android.content.ClipboardManager;
+import android.content.ClipboardManager.OnPrimaryClipChangedListener;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements
+    OnPrimaryClipChangedListener {
+  private ClipboardManager cm=null;
+  private TextView lastClip=null;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    lastClip=(TextView)findViewById(R.id.last_clip);
+    cm=(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+    cm.addPrimaryClipChangedListener(this);
+  }
+  
+  @Override
+  public void onDestroy() {
+    cm.removePrimaryClipChangedListener(this);
+    super.onDestroy();
+  }
+
+  @Override
+  public void onPrimaryClipChanged() {
+    lastClip.setText(cm.getPrimaryClip().getItemAt(0)
+                       .coerceToText(this));
+  }
+}

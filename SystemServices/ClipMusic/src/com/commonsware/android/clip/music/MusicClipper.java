98e6f75df9f342dad584b6a5943c5fa69349bc1b
Mark Murphy <mmurphy@commonsware.com>
2013-08-24 11:58:30 -0400

    exception handling and formatting

--- SystemServices/ClipMusic/src/com/commonsware/android/clip/music/MusicClipper.java
+++ SystemServices/ClipMusic/src/com/commonsware/android/clip/music/MusicClipper.java
@@ -10,7 +10,7 @@
   
   From _The Busy Coder's Guide to Android Development_
     http://commonsware.com/Android
-*/
+ */
 
 package com.commonsware.android.clip.music;
 
@@ -20,10 +20,11 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-public class MusicClipper extends Activity{
+public class MusicClipper extends Activity {
   private static final int PICK_REQUEST=1337;
   private ClipboardManager clipboard=null;
 
@@ -31,46 +32,56 @@ public class MusicClipper extends Activity{
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    
+
     clipboard=(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
   }
-  
+
   @Override
   protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-    if (requestCode==PICK_REQUEST) {
-      if (resultCode==RESULT_OK) {
-        ClipData clip=ClipData.newUri(getContentResolver(),
-                                       "Some music", data.getData());
-        
-        clipboard.setPrimaryClip(clip);
+                                  Intent data) {
+    if (requestCode == PICK_REQUEST) {
+      if (resultCode == RESULT_OK) {
+        ClipData clip=
+            ClipData.newUri(getContentResolver(), "Some music",
+                            data.getData());
+
+        try {
+          clipboard.setPrimaryClip(clip);
+        }
+        catch (Exception e) {
+          Log.e(getClass().getSimpleName(), "Exception clipping Uri", e);
+          Toast.makeText(this, "Exception: " + e.getMessage(),
+                         Toast.LENGTH_SHORT).show();
+        }
       }
     }
   }
-  
+
   public void pickMusic(View v) {
     Intent i=new Intent(Intent.ACTION_GET_CONTENT);
-    
+
     i.setType("audio/*");
     startActivityForResult(i, PICK_REQUEST);
   }
-  
+
   public void playMusic(View v) {
     ClipData clip=clipboard.getPrimaryClip();
-    
-    if (clip==null) {
-      Toast.makeText(this, "There is no clip!", Toast.LENGTH_LONG).show();
+
+    if (clip == null) {
+      Toast.makeText(this, "There is no clip!", Toast.LENGTH_LONG)
+           .show();
     }
     else {
       ClipData.Item item=clip.getItemAt(0);
       Uri song=item.getUri();
-      
-      if (song!=null &&
-          getContentResolver().getType(song).startsWith("audio/")) {
+
+      if (song != null
+          && getContentResolver().getType(song).startsWith("audio/")) {
         startActivity(new Intent(Intent.ACTION_VIEW, song));
       }
       else {
-        Toast.makeText(this, "There is no song!", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "There is no song!", Toast.LENGTH_LONG)
+             .show();
       }
     }
   }

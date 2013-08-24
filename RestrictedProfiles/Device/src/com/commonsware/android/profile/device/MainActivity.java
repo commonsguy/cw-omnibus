a68fd5b702bce0621947a33f6b9b69a550c50ff9
Mark Murphy <mmurphy@commonsware.com>
2013-08-24 11:59:52 -0400

    sample apps demonstrating restricted profiles

---
+++ RestrictedProfiles/Device/src/com/commonsware/android/profile/device/MainActivity.java
@@ -0,0 +1,44 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.profile.device;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.UserManager;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    UserManager mgr=(UserManager)getSystemService(USER_SERVICE);
+    Bundle restrictions=mgr.getUserRestrictions();
+
+    if (restrictions.keySet().size() > 0) {
+      setContentView(R.layout.activity_main);
+
+      RestrictionsFragment f=
+          (RestrictionsFragment)getFragmentManager().findFragmentById(R.id.contents);
+
+      f.showRestrictions(restrictions);
+    }
+    else {
+      Toast.makeText(this, R.string.no_restrictions, Toast.LENGTH_LONG)
+           .show();
+      finish();
+    }
+  }
+}
---
+++ RestrictedProfiles/Device/src/com/commonsware/android/profile/device/RestrictionsFragment.java
@@ -0,0 +1,60 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.profile.device;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RestrictionsFragment extends ListFragment {
+  public void showRestrictions(Bundle restrictions) {
+    setListAdapter(new RestrictionsAdapter(restrictions));
+  }
+
+  class RestrictionsAdapter extends ArrayAdapter<String> {
+    Bundle restrictions;
+
+    RestrictionsAdapter(Bundle restrictions) {
+      super(getActivity(), android.R.layout.simple_list_item_1,
+            new ArrayList<String>());
+
+      ArrayList<String> keys=
+          new ArrayList<String>(restrictions.keySet());
+
+      Collections.sort(keys);
+      addAll(keys);
+
+      this.restrictions=restrictions;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      TextView row=
+          (TextView)super.getView(position, convertView, parent);
+      int icon=
+          restrictions.getBoolean(getItem(position))
+              ? R.drawable.ic_true : R.drawable.ic_false;
+
+      row.setCompoundDrawablesWithIntrinsicBounds(0, 0, icon, 0);
+
+      return(row);
+    }
+  }
+}

91bf7298c478c9690028049cbfcc237deef64901
Mark Murphy <mmurphy@commonsware.com>
2013-09-03 18:03:16 -0400

    replaced stock pref fragment with simpler alternative

--- Prefs/FragmentsBC/src/com/commonsware/android/preffragsbc/EditPreferences.java
+++ Prefs/FragmentsBC/src/com/commonsware/android/preffragsbc/EditPreferences.java
@@ -1,5 +1,5 @@
 /***
-  Copyright (c) 2008-2012 CommonsWare, LLC
+  Copyright (c) 2008-2013 CommonsWare, LLC
   Licensed under the Apache License, Version 2.0 (the "License"); you may not
   use this file except in compliance with the License. You may obtain	a copy
   of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
@@ -15,8 +15,10 @@
 package com.commonsware.android.preffragsbc;
 
 import java.util.List;
+import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
 public class EditPreferences extends SherlockPreferenceActivity {
@@ -31,8 +33,29 @@ public class EditPreferences extends SherlockPreferenceActivity {
     }
   }
   
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
   @Override
   public void onBuildHeaders(List<Header> target) {
     loadHeadersFromResource(R.xml.preference_headers, target);
+  }
+  
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  public static class FirstPreferenceFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      addPreferencesFromResource(R.xml.preferences);
+    }
+  }
+  
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  public static class SecondPreferenceFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      addPreferencesFromResource(R.xml.preferences2);
+    }
   }
 }

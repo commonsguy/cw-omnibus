075d0c662d295409826976b418cfbcd39567cdfe
Mark Murphy <mmurphy@commonsware.com>
2013-09-23 14:37:38 -0400

    samples of examining, dumping, and checking signing keys

---
+++ MiscSecurity/SigDump/src/com/commonsware/android/signature/dump/SignatureFragment.java
@@ -0,0 +1,77 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.signature.dump;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class SignatureFragment extends SherlockFragment {
+  DateFormat fmt=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    return(inflater.inflate(R.layout.sig, container, false));
+  }
+
+  void show(byte[] raw) {
+    CertificateFactory cf=null;
+
+    try {
+      cf=CertificateFactory.getInstance("X509");
+    }
+    catch (CertificateException e) {
+      Log.e(getClass().getSimpleName(),
+            "Exception getting CertificateFactory", e);
+      return;
+    }
+
+    X509Certificate c=null;
+    ByteArrayInputStream bin=new ByteArrayInputStream(raw);
+
+    try {
+      c=(X509Certificate)cf.generateCertificate(bin);
+    }
+    catch (CertificateException e) {
+      Log.e(getClass().getSimpleName(),
+            "Exception getting X509Certificate", e);
+      return;
+    }
+
+    TextView tv=(TextView)getView().findViewById(R.id.subject);
+
+    tv.setText(c.getSubjectDN().toString());
+
+    tv=(TextView)getView().findViewById(R.id.issuer);
+    tv.setText(c.getIssuerDN().toString());
+
+    tv=(TextView)getView().findViewById(R.id.valid);
+    tv.setText(fmt.format(c.getNotBefore()) + " to "
+        + fmt.format(c.getNotAfter()));
+  }
+}

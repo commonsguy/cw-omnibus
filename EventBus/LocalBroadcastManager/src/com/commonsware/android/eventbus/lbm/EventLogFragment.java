b4155a799dde130399a3b33d9fedef8618b22508
Mark Murphy <mmurphy@commonsware.com>
2013-10-08 16:09:43 -0400

    event bus sample apps

---
+++ EventBus/LocalBroadcastManager/src/com/commonsware/android/eventbus/lbm/EventLogFragment.java
@@ -0,0 +1,103 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.eventbus.lbm;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import com.actionbarsherlock.app.SherlockListFragment;
+
+public class EventLogFragment extends SherlockListFragment {
+  static final String EXTRA_RANDOM="r";
+  static final String EXTRA_TIME="t";
+  static final String ACTION_EVENT="e";
+  private EventLogAdapter adapter=null;
+
+  @Override
+  public void onActivityCreated(Bundle state) {
+    super.onActivityCreated(state);
+
+    setRetainInstance(true);
+    getListView().setTranscriptMode(ListView.TRANSCRIPT_MODE_NORMAL);
+
+    if (adapter == null) {
+      adapter=new EventLogAdapter();
+    }
+
+    setListAdapter(adapter);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    IntentFilter filter=new IntentFilter(ACTION_EVENT);
+
+    LocalBroadcastManager.getInstance(getActivity())
+                         .registerReceiver(onEvent, filter);
+  }
+
+  @Override
+  public void onPause() {
+    LocalBroadcastManager.getInstance(getActivity())
+                         .unregisterReceiver(onEvent);
+
+    super.onPause();
+  }
+
+  class EventLogAdapter extends ArrayAdapter<Intent> {
+    DateFormat fmt=new SimpleDateFormat("HH:mm:ss", Locale.US);
+
+    public EventLogAdapter() {
+      super(getActivity(), android.R.layout.simple_list_item_1,
+            new ArrayList<Intent>());
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      TextView row=
+          (TextView)super.getView(position, convertView, parent);
+      Intent event=getItem(position);
+      Date date=new Date(event.getLongExtra(EXTRA_TIME, 0));
+
+      row.setText(String.format("%s = %x", fmt.format(date),
+                                event.getIntExtra(EXTRA_RANDOM, -1)));
+
+      return(row);
+    }
+  }
+
+  private BroadcastReceiver onEvent=new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      adapter.add(intent);
+    }
+  };
+}

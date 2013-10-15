0f90a03dee32e698f201a1b00978ded7d9b363f7
Mark Murphy <mmurphy@commonsware.com>
2013-10-15 07:44:28 -0400

    generated project to get a Gradle setup

---
+++ Gradle/Hello/build/source/buildConfig/debug/com/commonsware/android/gradle/hello/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.commonsware.android.gradle.hello;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}

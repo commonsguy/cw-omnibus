57bef250cf7551c1d358772b038f9f17cb34226b
Mark Murphy <mmurphy@commonsware.com>
2013-10-23 14:45:24 -0400

    another sample Presentation app, this time showing an actual presentation

---
+++ Presentation/Slides/src/com/commonsware/android/preso/slides/MainActivity.java
@@ -0,0 +1,131 @@
+/***
+  Copyright (c) 2013 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.preso.slides;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.commonsware.cwac.preso.PresentationHelper;
+import com.viewpagerindicator.TabPageIndicator;
+
+public class MainActivity extends Activity implements
+    PresentationHelper.Listener, OnPageChangeListener {
+  private ViewPager pager=null;
+  private SlidePresentationFragment preso=null;
+  private SlidesAdapter adapter=null;
+  private PresentationHelper helper=null;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    TabPageIndicator tabs=(TabPageIndicator)findViewById(R.id.titles);
+
+    pager=(ViewPager)findViewById(R.id.pager);
+    adapter=new SlidesAdapter(this);
+    pager.setAdapter(adapter);
+    tabs.setViewPager(pager);
+    tabs.setOnPageChangeListener(this);
+
+    helper=new PresentationHelper(this, this);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    helper.onResume();
+  }
+
+  @Override
+  public void onPause() {
+    helper.onPause();
+    super.onPause();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_actions, menu);
+
+    return(super.onCreateOptionsMenu(menu));
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.present:
+        boolean original=item.isChecked();
+
+        item.setChecked(!original);
+
+        if (original) {
+          helper.disable();
+        }
+        else {
+          helper.enable();
+        }
+
+        break;
+
+      case R.id.first:
+        pager.setCurrentItem(0);
+        break;
+
+      case R.id.last:
+        pager.setCurrentItem(adapter.getCount() - 1);
+        break;
+    }
+
+    return(super.onOptionsItemSelected(item));
+  }
+
+  @Override
+  public void onPageScrollStateChanged(int arg0) {
+    // ignored
+  }
+
+  @Override
+  public void onPageScrolled(int arg0, float arg1, int arg2) {
+    // ignored
+  }
+
+  @Override
+  public void onPageSelected(int position) {
+    if (preso != null) {
+      preso.setSlideContent(adapter.getPageResource(position));
+    }
+  }
+
+  @Override
+  public void clearPreso(boolean showInline) {
+    if (preso != null) {
+      preso.dismiss();
+      preso=null;
+    }
+  }
+
+  @Override
+  public void showPreso(Display display) {
+    preso=
+        SlidePresentationFragment.newInstance(this,
+                                              display,
+                                              adapter.getPageResource(pager.getCurrentItem()));
+    preso.show(getFragmentManager(), "preso");
+  }
+}

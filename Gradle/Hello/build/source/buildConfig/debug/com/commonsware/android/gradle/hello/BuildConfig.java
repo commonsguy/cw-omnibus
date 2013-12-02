fb2d686940c0d639bc5e0556375bcabeb1bb2083
Mark Murphy <mmurphy@commonsware.com>
2013-12-02 08:55:41 -0500

    removed binary output

--- Gradle/Hello/build/source/buildConfig/debug/com/commonsware/android/gradle/hello/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.commonsware.android.gradle.hello;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}

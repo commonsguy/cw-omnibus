0159f63e9eec08c989afc677b57701a1baca7626
Mark Murphy <mmurphy@commonsware.com>
2013-12-02 08:56:33 -0500

    Gradle sub-projects demo

---
+++ Gradle/HelloMultiProject/libraries/HelloLibrary/src/main/java/com/commonsware/android/gradle/hellolib/SomeUtils.java
@@ -0,0 +1,9 @@
+package com.commonsware.android.gradle.hellolib;
+
+import android.util.Log;
+
+public class SomeUtils {
+  static public void doSomething() {
+    Log.d("SomeUtils", "doSomething() was called!");
+  }
+}

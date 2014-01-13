b487971eb4e1ff0ac09cdbd0e8e355a88770eba2
Mark Murphy <mmurphy@commonsware.com>
2014-01-13 08:41:55 -0500

    updated for Android 4.4

--- WebKit/GeoWeb2/src/com/commonsware/android/geoweb2/GeoWebTwo.java
+++ WebKit/GeoWeb2/src/com/commonsware/android/geoweb2/GeoWebTwo.java
@@ -10,16 +10,18 @@
   
   From _The Busy Coder's Guide to Android Development_
     http://commonsware.com/Android
-*/
+ */
 
 package com.commonsware.android.geoweb2;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
@@ -30,75 +32,80 @@ public class GeoWebTwo extends Activity {
   private static String PROVIDER="gps";
   private WebView browser;
   private LocationManager myLocationManager=null;
-  
+
   @SuppressLint("SetJavaScriptEnabled")
   @Override
   public void onCreate(Bundle icicle) {
     super.onCreate(icicle);
     setContentView(R.layout.main);
     browser=(WebView)findViewById(R.id.webkit);
-    
-    myLocationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-    
+
+    myLocationManager=
+        (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
     browser.getSettings().setJavaScriptEnabled(true);
     browser.addJavascriptInterface(new Locater(), "locater");
     browser.loadUrl("file:///android_asset/geoweb2.html");
   }
-  
+
   @Override
   public void onResume() {
     super.onResume();
-    myLocationManager.requestLocationUpdates(PROVIDER, 0,
-                                              0,
-                                              onLocation);
+    myLocationManager.requestLocationUpdates(PROVIDER, 0, 0, onLocation);
   }
-  
+
   @Override
   public void onPause() {
     super.onPause();
     myLocationManager.removeUpdates(onLocation);
   }
-  
+
   LocationListener onLocation=new LocationListener() {
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     public void onLocationChanged(Location location) {
-      StringBuilder buf=new StringBuilder("javascript:whereami(");
-      
+      StringBuilder buf=new StringBuilder("whereami(");
+
       buf.append(String.valueOf(location.getLatitude()));
       buf.append(",");
       buf.append(String.valueOf(location.getLongitude()));
       buf.append(")");
-      
-      browser.loadUrl(buf.toString());
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        browser.evaluateJavascript(buf.toString(), null);
+      }
+      else {
+        browser.loadUrl("javascript:" + buf.toString());
+      }
     }
-    
+
     public void onProviderDisabled(String provider) {
       // required for interface, not used
     }
-    
+
     public void onProviderEnabled(String provider) {
       // required for interface, not used
     }
-    
+
     public void onStatusChanged(String provider, int status,
-                                  Bundle extras) {
+                                Bundle extras) {
       // required for interface, not used
     }
   };
-  
+
   public class Locater {
     @JavascriptInterface
     public String getLocation() throws JSONException {
       Location loc=myLocationManager.getLastKnownLocation(PROVIDER);
-      
-      if (loc==null) {
+
+      if (loc == null) {
         return(null);
       }
-      
+
       JSONObject json=new JSONObject();
 
       json.put("lat", loc.getLatitude());
       json.put("lon", loc.getLongitude());
-      
+
       return(json.toString());
     }
   }

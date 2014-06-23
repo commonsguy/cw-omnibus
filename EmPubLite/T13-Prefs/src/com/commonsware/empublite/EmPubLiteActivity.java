431b3bd802c6385e14baea2e9ed07359edf3dec1
Mark Murphy <mmurphy@commonsware.com>
2014-06-23 10:13:19 -0400

    updated based on T12 and new instructions

--- EmPubLite/T13-Prefs/src/com/commonsware/empublite/EmPubLiteActivity.java
+++ EmPubLite/T13-Prefs/src/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,16 +1,16 @@
 package com.commonsware.empublite;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
-import com.actionbarsherlock.view.MenuItem;
+import de.greenrobot.event.EventBus;
 
-public class EmPubLiteActivity extends SherlockFragmentActivity {
+public class EmPubLiteActivity extends Activity {
   private static final String MODEL="model";
   private static final String PREF_LAST_POSITION="lastPosition";
   private static final String PREF_SAVE_LAST_POSITION=
@@ -18,44 +18,59 @@ public class EmPubLiteActivity extends SherlockFragmentActivity {
   private static final String PREF_KEEP_SCREEN_ON="keepScreenOn";
   private ViewPager pager=null;
   private ContentsAdapter adapter=null;
-  private SharedPreferences prefs=null;
+  private ModelFragment mfrag=null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    if (getSupportFragmentManager().findFragmentByTag(MODEL) == null) {
-      getSupportFragmentManager().beginTransaction()
-                                 .add(new ModelFragment(), MODEL)
-                                 .commit();
-    }
-
     setContentView(R.layout.main);
-
     pager=(ViewPager)findViewById(R.id.pager);
-    getSupportActionBar().setHomeButtonEnabled(true);
-  }
 
-  @Override
-  public void onPause() {
-    if (prefs != null) {
-      int position=pager.getCurrentItem();
-      prefs.edit().putInt(PREF_LAST_POSITION, position).apply();
+    mfrag=(ModelFragment)getFragmentManager().findFragmentByTag(MODEL);
+
+    if (mfrag == null) {
+      mfrag=new ModelFragment();
+      getFragmentManager().beginTransaction().add(mfrag, MODEL)
+                          .commit();
     }
-    super.onPause();
+    else if (mfrag.getBook() != null) {
+      setupPager(mfrag.getBook());
+    }
+
+    getActionBar().setHomeButtonEnabled(true);
   }
 
   @Override
   public void onResume() {
     super.onResume();
-    if (prefs != null) {
-      pager.setKeepScreenOn(prefs.getBoolean(PREF_KEEP_SCREEN_ON, false));
+
+    EventBus.getDefault().register(this);
+
+    if (mfrag.getPrefs() != null) {
+      pager.setKeepScreenOn(mfrag.getPrefs()
+                                 .getBoolean(PREF_KEEP_SCREEN_ON, false));
     }
   }
 
   @Override
+  public void onPause() {
+    EventBus.getDefault().unregister(this);
+
+    if (mfrag.getPrefs() != null) {
+      int position=pager.getCurrentItem();
+
+      mfrag.getPrefs().edit().putInt(PREF_LAST_POSITION, position)
+           .apply();
+    }
+
+    super.onPause();
+  }
+
+  @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    new MenuInflater(this).inflate(R.menu.options, menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+
     return(super.onCreateOptionsMenu(menu));
   }
 
@@ -77,9 +92,9 @@ public class EmPubLiteActivity extends SherlockFragmentActivity {
 
       case R.id.help:
         i=new Intent(this, SimpleContentActivity.class);
+
         i.putExtra(SimpleContentActivity.EXTRA_FILE,
                    "file:///android_asset/misc/help.html");
-
         startActivity(i);
 
         return(true);
@@ -92,19 +107,25 @@ public class EmPubLiteActivity extends SherlockFragmentActivity {
     return(super.onOptionsItemSelected(item));
   }
 
-  void setupPager(SharedPreferences prefs, BookContents contents) {
-    this.prefs=prefs;
+  public void onEventMainThread(BookLoadedEvent event) {
+    setupPager(event.getBook());
+  }
 
+  private void setupPager(BookContents contents) {
     adapter=new ContentsAdapter(this, contents);
     pager.setAdapter(adapter);
 
     findViewById(R.id.progressBar1).setVisibility(View.GONE);
     findViewById(R.id.pager).setVisibility(View.VISIBLE);
 
-    if (prefs.getBoolean(PREF_SAVE_LAST_POSITION, false)) {
-      pager.setCurrentItem(prefs.getInt(PREF_LAST_POSITION, 0));
-    }
+    SharedPreferences prefs=mfrag.getPrefs();
 
-    pager.setKeepScreenOn(prefs.getBoolean(PREF_KEEP_SCREEN_ON, false));
+    if (prefs != null) {
+      if (prefs.getBoolean(PREF_SAVE_LAST_POSITION, false)) {
+        pager.setCurrentItem(prefs.getInt(PREF_LAST_POSITION, 0));
+      }
+
+      pager.setKeepScreenOn(prefs.getBoolean(PREF_KEEP_SCREEN_ON, false));
+    }
   }
 }

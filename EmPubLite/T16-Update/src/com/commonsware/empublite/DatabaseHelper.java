4a532631bcc00ff20f5889257fc84b3c526e772c
Mark Murphy <mmurphy@commonsware.com>
2014-06-26 09:01:57 -0400

    updated based on T15 and new instructions

--- EmPubLite/T16-Update/src/com/commonsware/empublite/DatabaseHelper.java
+++ EmPubLite/T16-Update/src/com/commonsware/empublite/DatabaseHelper.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.AsyncTask;
+import android.os.Process;
+import de.greenrobot.event.EventBus;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
   private static final String DATABASE_NAME="empublite.db";
   private static final int SCHEMA_VERSION=1;
   private static DatabaseHelper singleton=null;
-  private Context ctxt=null;
 
   synchronized static DatabaseHelper getInstance(Context ctxt) {
     if (singleton == null) {
@@ -22,107 +22,71 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
   private DatabaseHelper(Context ctxt) {
     super(ctxt, DATABASE_NAME, null, SCHEMA_VERSION);
-    this.ctxt=ctxt;
   }
 
   @Override
   public void onCreate(SQLiteDatabase db) {
-    try {
-      db.beginTransaction();
-      db.execSQL("CREATE TABLE notes (position INTEGER PRIMARY KEY, prose TEXT);");
-      db.setTransactionSuccessful();
-    }
-    finally {
-      db.endTransaction();
-    }
+    db.execSQL("CREATE TABLE notes (position INTEGER PRIMARY KEY, prose TEXT);");
   }
 
   @Override
   public void onUpgrade(SQLiteDatabase db, int oldVersion,
                         int newVersion) {
-    throw new RuntimeException(
-                               ctxt.getString(R.string.on_upgrade_error));
+    throw new RuntimeException("This should not be called");
   }
 
-  void getNoteAsync(int position, NoteListener listener) {
-    ModelFragment.executeAsyncTask(new GetNoteTask(listener), position);
+  void loadNote(int position) {
+    new LoadThread(position).start();
   }
 
-  void saveNoteAsync(int position, String note) {
-    ModelFragment.executeAsyncTask(new SaveNoteTask(position, note));
+  void updateNote(int position, String prose) {
+    new UpdateThread(position, prose).start();
   }
 
-  void deleteNoteAsync(int position) {
-    ModelFragment.executeAsyncTask(new DeleteNoteTask(), position);
-  }
+  private class LoadThread extends Thread {
+    private int position=-1;
 
-  interface NoteListener {
-    void setNote(String note);
-  }
+    LoadThread(int position) {
+      super();
 
-  private class GetNoteTask extends AsyncTask<Integer, Void, String> {
-    private NoteListener listener=null;
-
-    GetNoteTask(NoteListener listener) {
-      this.listener=listener;
+      this.position=position;
+      Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
     }
 
     @Override
-    protected String doInBackground(Integer... params) {
-      String[] args= { params[0].toString() };
-
+    public void run() {
+      String[] args= { String.valueOf(position) };
       Cursor c=
-          getReadableDatabase().rawQuery("SELECT prose FROM notes WHERE position=?",
+          getReadableDatabase().rawQuery("SELECT prose FROM notes WHERE position = ?",
                                          args);
-
-      c.moveToFirst();
-
-      if (c.isAfterLast()) {
-        return(null);
+      if (c.getCount() > 0) {
+        c.moveToFirst();
+        EventBus.getDefault().post(new NoteLoadedEvent(position,
+                                                       c.getString(0)));
       }
 
-      String result=c.getString(0);
-
       c.close();
-
-      return(result);
-    }
-
-    @Override
-    public void onPostExecute(String prose) {
-      listener.setNote(prose);
     }
   }
 
-  private class SaveNoteTask extends AsyncTask<Void, Void, Void> {
-    private int position;
-    private String note=null;
+  private class UpdateThread extends Thread {
+    private int position=-1;
+    private String prose=null;
 
-    SaveNoteTask(int position, String note) {
+    UpdateThread(int position, String prose) {
+      super();
+
       this.position=position;
-      this.note=note;
+      this.prose=prose;
+      Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
     }
 
     @Override
-    protected Void doInBackground(Void... params) {
-      String[] args= { String.valueOf(position), note };
+    public void run() {
+      String[] args= { String.valueOf(position), prose };
 
       getWritableDatabase().execSQL("INSERT OR REPLACE INTO notes (position, prose) VALUES (?, ?)",
                                     args);
-
-      return(null);
-    }
-  }
-
-  private class DeleteNoteTask extends AsyncTask<Integer, Void, Void> {
-    @Override
-    protected Void doInBackground(Integer... params) {
-      String[] args= { params[0].toString() };
-
-      getWritableDatabase().execSQL("DELETE FROM notes WHERE position=?",
-                                    args);
-
-      return(null);
     }
   }
 }
--- EmPubLite/T16-Update/src/com/commonsware/empublite/NoteActivity.java
+++ EmPubLite/T16-Update/src/com/commonsware/empublite/NoteActivity.java
@@ -1,42 +1,30 @@
 package com.commonsware.empublite;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-public class NoteActivity extends SherlockFragmentActivity {
+public class NoteActivity extends Activity implements
+    NoteFragment.Contract {
   public static final String EXTRA_POSITION="position";
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    if (getSupportFragmentManager().findFragmentById(android.R.id.content)==null) {
+    if (getFragmentManager().findFragmentById(android.R.id.content) == null) {
       int position=getIntent().getIntExtra(EXTRA_POSITION, -1);
 
       if (position >= 0) {
         Fragment f=NoteFragment.newInstance(position);
 
-        getSupportFragmentManager().beginTransaction()
-                                   .add(android.R.id.content, f)
-                                   .commit();
+        getFragmentManager().beginTransaction()
+                            .add(android.R.id.content, f).commit();
       }
     }
   }
-
-  void closeNotes() {
+  
+  public void closeNotes() {
     finish();
-  }
-
-  void sendNotes(String prose) {
-    Intent i=new Intent(Intent.ACTION_SEND);
-
-    i.setType("text/plain");
-    i.putExtra(Intent.EXTRA_TEXT, prose);
-
-    startActivity(Intent.createChooser(i,
-                                       getString(R.string.share_title)));
-
   }
 }

4ac4751bb4ea2214ffa259d86631734c7d40eef9
Mark Murphy <mmurphy@commonsware.com>
2014-07-01 10:58:33 -0400

    new greenrobot EventBus demo

---
+++ EventBus/AsyncDemo/src/com/commonsware/android/eventbus/AsyncDemoFragment.java
@@ -0,0 +1,61 @@
+/***
+  Copyright (c) 2008-2014 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.eventbus;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import de.greenrobot.event.EventBus;
+
+public class AsyncDemoFragment extends ListFragment {
+  private ArrayAdapter<String> adapter=null;
+  private ArrayList<String> model=null;
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    adapter=
+        new ArrayAdapter<String>(getActivity(),
+                                 android.R.layout.simple_list_item_1,
+                                 model);
+
+    getListView().setScrollbarFadingEnabled(false);
+    setListAdapter(adapter);
+  }
+
+  @Override
+  public void onAttach(Activity activity) {
+    super.onAttach(activity);
+
+    EventBus.getDefault().register(this);
+  }
+
+  @Override
+  public void onDetach() {
+    EventBus.getDefault().unregister(this);
+
+    super.onDetach();
+  }
+
+  public void onEventMainThread(WordReadyEvent event) {
+    adapter.notifyDataSetChanged();
+  }
+
+  public void setModel(ArrayList<String> model) {
+    this.model=model;
+  }
+}
---
+++ EventBus/AsyncDemo/src/com/commonsware/android/eventbus/ModelFragment.java
@@ -0,0 +1,60 @@
+/***
+  Copyright (c) 2008-2014 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.eventbus;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.SystemClock;
+import java.util.ArrayList;
+import de.greenrobot.event.EventBus;
+
+public class ModelFragment extends Fragment {
+  private static final String[] items= { "lorem", "ipsum", "dolor",
+      "sit", "amet", "consectetuer", "adipiscing", "elit", "morbi",
+      "vel", "ligula", "vitae", "arcu", "aliquet", "mollis", "etiam",
+      "vel", "erat", "placerat", "ante", "porttitor", "sodales",
+      "pellentesque", "augue", "purus" };
+  private ArrayList<String> model=new ArrayList<String>();
+  private boolean isStarted=false;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setRetainInstance(true);
+
+    if (!isStarted) {
+      isStarted=true;
+      new LoadWordsThread().start();
+    }
+  }
+  
+  public ArrayList<String> getModel() {
+    return(model);
+  }
+
+  class LoadWordsThread extends Thread {
+    @Override
+    public void run() {
+      for (String item : items) {
+        if (!isInterrupted()) {
+          model.add(item);
+          EventBus.getDefault().post(new WordReadyEvent());
+          SystemClock.sleep(400);
+        }
+      }
+    }
+  }
+}

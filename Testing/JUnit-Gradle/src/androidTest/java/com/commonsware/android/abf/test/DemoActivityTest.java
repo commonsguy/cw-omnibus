c7bb93d9652c20084b53ef1707d0bb19c06ac12c
Mark Murphy <mmurphy@commonsware.com>
2014-09-03 09:02:06 -0400

    new Android Studio/Gradle editions of existing samples

---
+++ Testing/JUnit-Gradle/src/androidTest/java/com/commonsware/android/abf/test/DemoActivityTest.java
@@ -0,0 +1,55 @@
+/***
+	Copyright (c) 2008-2014 CommonsWare, LLC
+	Licensed under the Apache License, Version 2.0 (the "License"); you may not
+	use this file except in compliance with the License. You may obtain a copy
+	of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+	by applicable law or agreed to in writing, software distributed under the
+	License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+	OF ANY KIND, either express or implied. See the License for the specific
+	language governing permissions and limitations under the License.
+	
+	From _The Busy Coder's Guide to Android Development_
+		http://commonsware.com/Android
+ */
+
+package com.commonsware.android.abf.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.ListView;
+import com.commonsware.android.abf.ActionBarFragmentActivity;
+
+public class DemoActivityTest extends
+    ActivityInstrumentationTestCase2<ActionBarFragmentActivity> {
+  private ListView list=null;
+
+  public DemoActivityTest() {
+    super(ActionBarFragmentActivity.class);
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    setActivityInitialTouchMode(false);
+
+    ActionBarFragmentActivity activity=getActivity();
+
+    list=(ListView)activity.findViewById(android.R.id.list);
+  }
+
+  public void testListCount() {
+    assertEquals(list.getAdapter().getCount(), 25);
+  }
+
+  public void testKeyEvents() {
+    sendKeys("4*DPAD_DOWN");
+    assertEquals(list.getSelectedItemPosition(), 4);
+  }
+
+  public void testTouchEvents() {
+    TouchUtils.scrollToBottom(this, getActivity(), list);
+    getInstrumentation().waitForIdleSync();
+    assertEquals(list.getLastVisiblePosition(), 24);
+  }
+}

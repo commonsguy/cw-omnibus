4233d13eb46e6f0beb250ca19f5e0eccb4327c1e
Mark Murphy <mmurphy@commonsware.com>
2015-01-20 10:34:47 -0500

    new samples

---
+++ Testing/AndroidJUnitRunner/src/androidTest/java/com/commonsware/android/abf/test/DemoContextTest.java
@@ -0,0 +1,47 @@
+/***
+	Copyright (c) 2008-2012 CommonsWare, LLC
+	Licensed under the Apache License, Version 2.0 (the "License"); you may not
+	use this file except in compliance with the License. You may obtain a copy
+	of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+	by applicable law or agreed to in writing, software distributed under the
+	License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+	OF ANY KIND, either express or implied. See the License for the specific
+	language governing permissions and limitations under the License.
+	
+	From _The Busy Coder's Guide to Android Development_
+		http://commonsware.com/Android
+ */
+
+package com.commonsware.android.abf.test;
+
+import android.test.AndroidTestCase;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.commonsware.android.abf.R;
+
+public class DemoContextTest extends AndroidTestCase {
+  private View field=null;
+  private View root=null;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    LayoutInflater inflater=LayoutInflater.from(getContext());
+
+    root=inflater.inflate(R.layout.add, null);
+    root.measure(800, 480);
+    root.layout(0, 0, 800, 480);
+
+    field=root.findViewById(R.id.title);
+  }
+
+  public void testExists() {
+    assertNotNull(field);
+  }
+
+  public void testPosition() {
+    assertEquals(0, field.getTop());
+    assertEquals(0, field.getLeft());
+  }
+}

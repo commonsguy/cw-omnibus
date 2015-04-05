794959b6d3b81eafe1aeefb6b8636688acc29e6c
Mark Murphy <mmurphy@commonsware.com>
2015-04-05 09:37:40 -0400

    sample for playing with tasks

---
+++ Tasks/TaskCanary/app/src/main/java/com/commonsware/android/task/canary/OtherActivity.java
@@ -0,0 +1,65 @@
+/***
+ Copyright (c) 2015 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain	a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+
+ From _The Busy Coder's Guide to Android Development_
+ http://commonsware.com/Android
+ */
+
+package com.commonsware.android.task.canary;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+public class OtherActivity extends Activity {
+  @Override
+  public void onCreate(Bundle state) {
+    super.onCreate(state);
+    setContentView(R.layout.other);
+  }
+
+  public void firstTop(View v) {
+    startActivity(new Intent(this, MainActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+  }
+
+  public void firstTask(View v) {
+    startActivity(new Intent(this, MainActivity.class)
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+  }
+
+  public void secondTop(View v) {
+    startActivity(new Intent(this, OtherActivity.class)
+        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+  }
+
+  public void secondTask(View v) {
+    startActivity(new Intent(this, OtherActivity.class)
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+  }
+
+  public void settingsTop(View v) {
+    startActivity(new Intent(Settings.ACTION_DATE_SETTINGS)
+        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+  }
+
+  public void settingsTask(View v) {
+    startActivity(new Intent(Settings.ACTION_DATE_SETTINGS)
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+  }
+}

ddab4ec0cd911280f91b6a6eecc1bc355f018175
Mark Murphy <mmurphy@commonsware.com>
2015-05-25 10:00:18 -0400

    new sample showing sorted list

---
+++ RecyclerView/SortedList/src/com/commonsware/android/recyclerview/sorted/SortedFragment.java
@@ -0,0 +1,152 @@
+/***
+  Copyright (c) 2008-2015 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+*/
+
+package com.commonsware.android.recyclerview.sorted;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class SortedFragment extends RecyclerViewFragment {
+  private static final String[] items={"lorem", "ipsum", "dolor",
+          "sit", "amet",
+          "consectetuer", "adipiscing", "elit", "morbi", "vel",
+          "ligula", "vitae", "arcu", "aliquet", "mollis",
+          "etiam", "vel", "erat", "placerat", "ante",
+          "porttitor", "sodales", "pellentesque", "augue", "purus"};
+  private SortedList<String> model=null;
+  private AddStringTask task=null;
+  private IconicAdapter adapter=null;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setRetainInstance(true);
+
+    model=new SortedList<String>(String.class, sortCallback);
+
+    task=new AddStringTask();
+    task.execute();
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    setLayoutManager(new LinearLayoutManager(getActivity()));
+    adapter=new IconicAdapter();
+    setAdapter(adapter);
+  }
+
+  @Override
+  public void onDestroy() {
+    if (task != null) {
+      task.cancel(false);
+    }
+
+    super.onDestroy();
+  }
+
+  private SortedList.Callback<String> sortCallback=new SortedList.Callback<String>() {
+    @Override
+    public int compare(String o1, String o2) {
+      return o1.compareTo(o2);
+    }
+
+    @Override
+    public boolean areContentsTheSame(String oldItem, String newItem) {
+      return(areItemsTheSame(oldItem, newItem));
+    }
+
+    @Override
+    public boolean areItemsTheSame(String oldItem, String newItem) {
+      return(compare(oldItem, newItem)==0);
+    }
+
+    @Override
+    public void onInserted(int position, int count) {
+      adapter.notifyItemRangeInserted(position, count);
+    }
+
+    @Override
+    public void onRemoved(int position, int count) {
+      adapter.notifyItemRangeRemoved(position, count);
+    }
+
+    @Override
+    public void onMoved(int fromPosition, int toPosition) {
+      adapter.notifyItemMoved(fromPosition, toPosition);
+    }
+
+    @Override
+    public void onChanged(int position, int count) {
+      adapter.notifyItemRangeChanged(position, count);
+    }
+  };
+
+  class IconicAdapter extends RecyclerView.Adapter<RowController> {
+    @Override
+    public RowController onCreateViewHolder(ViewGroup parent, int viewType) {
+      return(new RowController(getActivity().getLayoutInflater()
+                                .inflate(R.layout.row, parent, false)));
+    }
+
+    @Override
+    public void onBindViewHolder(RowController holder, int position) {
+      holder.bindModel(model.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+      return(model.size());
+    }
+  }
+
+  private class AddStringTask extends AsyncTask<Void, String, Void> {
+    @Override
+    protected Void doInBackground(Void... unused) {
+      for (String item : items) {
+        if (isCancelled())
+          break;
+
+        publishProgress(item);
+        SystemClock.sleep(400);
+      }
+
+      return(null);
+    }
+
+    @Override
+    protected void onProgressUpdate(String... item) {
+      if (!isCancelled()) {
+        model.add(item[0]);
+      }
+    }
+
+    @Override
+    protected void onPostExecute(Void unused) {
+      Toast.makeText(getActivity(), R.string.done, Toast.LENGTH_SHORT)
+          .show();
+
+      task=null;
+    }
+  }
+}

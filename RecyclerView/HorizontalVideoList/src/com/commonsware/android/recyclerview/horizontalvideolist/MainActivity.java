48e359fa1003ebcd70517fc905b7b17843c1eda2
Mark Murphy <mmurphy@commonsware.com>
2015-06-17 11:59:16 -0400

    bug fix

--- RecyclerView/HorizontalVideoList/src/com/commonsware/android/recyclerview/horizontalvideolist/MainActivity.java
+++ RecyclerView/HorizontalVideoList/src/com/commonsware/android/recyclerview/horizontalvideolist/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends RecyclerViewActivity implements
 
     void setVideos(Cursor videos) {
       this.videos=videos;
+      notifyDataSetChanged();
     }
 
     @Override

97182e8506df5a0f4ccb148ba1ac253f15ff0b95
Mark Murphy <mmurphy@commonsware.com>
2015-08-06 07:32:42 -0400

    material tabs demos

---
+++ DesignSupport/TabLayoutPizza/app/src/main/java/com/commonsware/android/tablayout/pizza/EditorFragment.java
@@ -0,0 +1,54 @@
+/***
+  Copyright (c) 2012-14 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+ */
+
+package com.commonsware.android.tablayout.pizza;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class EditorFragment extends Fragment {
+  private static final String KEY_POSITION="position";
+
+  static EditorFragment newInstance(int position) {
+    EditorFragment frag=new EditorFragment();
+    Bundle args=new Bundle();
+
+    args.putInt(KEY_POSITION, position);
+    frag.setArguments(args);
+
+    return(frag);
+  }
+
+  static String getTitle(Context ctxt, int position) {
+    return(String.format(ctxt.getString(R.string.hint), position + 1));
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    View result=inflater.inflate(R.layout.editor, container, false);
+    EditText editor=(EditText)result.findViewById(R.id.editor);
+    int position=getArguments().getInt(KEY_POSITION, -1);
+
+    editor.setHint(getTitle(getActivity(), position));
+
+    return(result);
+  }
+}
---
+++ DesignSupport/TabLayoutPizza/app/src/main/java/com/commonsware/android/tablayout/pizza/SampleAdapter.java
@@ -0,0 +1,50 @@
+/***
+  Copyright (c) 2012-15 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+ */
+
+package com.commonsware.android.tablayout.pizza;
+
+import android.content.Context;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+
+public class SampleAdapter extends FragmentPagerAdapter {
+  private final Context ctxt;
+  private int pageCount=3;
+
+  public SampleAdapter(Context ctxt, FragmentManager mgr) {
+    super(mgr);
+
+    this.ctxt=ctxt;
+  }
+
+  @Override
+  public int getCount() {
+    return(pageCount);
+  }
+
+  @Override
+  public Fragment getItem(int position) {
+    return(EditorFragment.newInstance(position));
+  }
+
+  @Override
+  public String getPageTitle(int position) {
+    return(EditorFragment.getTitle(ctxt, position));
+  }
+
+  void setPageCount(int pageCount) {
+    this.pageCount=pageCount;
+  }
+}

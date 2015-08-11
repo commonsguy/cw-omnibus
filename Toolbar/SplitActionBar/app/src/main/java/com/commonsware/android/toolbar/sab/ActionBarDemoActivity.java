d6dd7b32d6e94f958ac56732d849b5536be9c153
Mark Murphy <mmurphy@commonsware.com>
2015-08-11 12:00:28 -0400

    demo of a split action bar workalike via a Toolbar

---
+++ Toolbar/SplitActionBar/app/src/main/java/com/commonsware/android/toolbar/sab/ActionBarDemoActivity.java
@@ -0,0 +1,110 @@
+/***
+  Copyright (c) 2008-2012 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+ */
+
+package com.commonsware.android.toolbar.sab;
+
+import android.app.ListActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import android.widget.Toolbar;
+import java.util.ArrayList;
+
+public class ActionBarDemoActivity extends ListActivity {
+  private static final String[] items= { "lorem", "ipsum", "dolor",
+      "sit", "amet", "consectetuer", "adipiscing", "elit", "morbi",
+      "vel", "ligula", "vitae", "arcu", "aliquet", "mollis", "etiam",
+      "vel", "erat", "placerat", "ante", "porttitor", "sodales",
+      "pellentesque", "augue", "purus" };
+  private ArrayList<String> words=null;
+  private ArrayAdapter<String> adapter=null;
+
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+
+    if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+      setContentView(R.layout.main);
+    }
+
+    initAdapter();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+      Toolbar tb=(Toolbar)findViewById(R.id.toolbar);
+
+      tb.inflateMenu(R.menu.actions);
+      tb.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+          return(onOptionsItemSelected(item));
+        }
+      });
+    }
+    else {
+      getMenuInflater().inflate(R.menu.actions, menu);
+    }
+
+    return(super.onCreateOptionsMenu(menu));
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch(item.getItemId()) {
+      case R.id.add:
+        addWord();
+
+        return(true);
+
+      case R.id.reset:
+        initAdapter();
+
+        return(true);
+
+      case R.id.about:
+        Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG)
+             .show();
+
+        return(true);
+    }
+
+    return(super.onOptionsItemSelected(item));
+  }
+
+  private void initAdapter() {
+    words=new ArrayList<String>();
+
+    for (int i=0;i<5;i++) {
+      words.add(items[i]);
+    }
+
+    adapter=
+        new ArrayAdapter<String>(this,
+                                 android.R.layout.simple_list_item_1,
+                                 words);
+
+    setListAdapter(adapter);
+  }
+
+  private void addWord() {
+    if (adapter.getCount()<items.length) {
+      adapter.add(items[adapter.getCount()]);
+    }
+  }
+}

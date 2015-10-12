880e9e3682a5f455ac5e9266c3de17c18853f414
Mark Murphy <mmurphy@commonsware.com>
2015-10-12 11:18:58 -0400

    new sample

---
+++ Introspection/ProcessText/app/src/main/java/com/commonsware/android/processtext/MainActivity.java
@@ -0,0 +1,70 @@
+/***
+  Copyright (c) 2013-2014 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+ */
+
+package com.commonsware.android.processtext;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import de.greenrobot.event.EventBus;
+
+public class MainActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (getFragmentManager().findFragmentById(android.R.id.content)==null) {
+      String search=null;
+
+      if (Intent.ACTION_PROCESS_TEXT.equals(getIntent().getAction())) {
+        search=getIntent().getStringExtra(Intent.EXTRA_PROCESS_TEXT);
+
+        if (search==null) {
+          search=getIntent().getStringExtra(Intent.EXTRA_PROCESS_TEXT_READONLY);
+        }
+      }
+
+      getFragmentManager()
+        .beginTransaction()
+        .add(android.R.id.content, QuestionsFragment.newInstance(search))
+        .commit();
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    EventBus.getDefault().register(this);
+  }
+
+  @Override
+  public void onPause() {
+    EventBus.getDefault().unregister(this);
+    super.onPause();
+  }
+
+  public void onEventMainThread(QuestionClickedEvent event) {
+    if (Intent.ACTION_PROCESS_TEXT.equals(getIntent().getAction()) &&
+      getIntent().getStringExtra(Intent.EXTRA_PROCESS_TEXT)!=null) {
+      setResult(Activity.RESULT_OK,
+        new Intent().putExtra(Intent.EXTRA_PROCESS_TEXT, event.item.link));
+      finish();
+    }
+    else {
+      startActivity(new Intent(Intent.ACTION_VIEW,
+        Uri.parse(event.item.link)));
+    }
+  }
+}
---
+++ Introspection/ProcessText/app/src/main/java/com/commonsware/android/processtext/QuestionsFragment.java
@@ -0,0 +1,120 @@
+/***
+  Copyright (c) 2013-2014 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+ */
+
+package com.commonsware.android.processtext;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.List;
+import com.squareup.picasso.Picasso;
+import de.greenrobot.event.EventBus;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class QuestionsFragment extends ListFragment implements
+    Callback<SOQuestions> {
+  private static final String ARG_SEARCH="search";
+
+  static QuestionsFragment newInstance(String search) {
+    QuestionsFragment result=new QuestionsFragment();
+    Bundle args=new Bundle();
+
+    args.putString(ARG_SEARCH, search);
+    result.setArguments(args);
+
+    return(result);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           ViewGroup container,
+                           Bundle savedInstanceState) {
+    View result=
+        super.onCreateView(inflater, container, savedInstanceState);
+
+    setRetainInstance(true);
+
+    RestAdapter restAdapter=
+        new RestAdapter.Builder().setEndpoint("https://api.stackexchange.com")
+                                 .build();
+    StackOverflowInterface so=
+        restAdapter.create(StackOverflowInterface.class);
+    String search=getArguments().getString(ARG_SEARCH);
+
+    if (search==null) {
+      so.questions("android", this);
+    }
+    else {
+      so.search(search, this);
+    }
+
+    return(result);
+  }
+
+  @Override
+  public void onListItemClick(ListView l, View v, int position, long id) {
+    Item item=((ItemsAdapter)getListAdapter()).getItem(position);
+
+    EventBus.getDefault().post(new QuestionClickedEvent(item));
+  }
+
+  @Override
+  public void failure(RetrofitError exception) {
+    Toast.makeText(getActivity(), exception.getMessage(),
+                   Toast.LENGTH_LONG).show();
+    Log.e(getClass().getSimpleName(),
+          "Exception from Retrofit request to StackOverflow", exception);
+  }
+
+  @Override
+  public void success(SOQuestions questions, Response response) {
+    setListAdapter(new ItemsAdapter(questions.items));
+  }
+
+  class ItemsAdapter extends ArrayAdapter<Item> {
+    ItemsAdapter(List<Item> items) {
+      super(getActivity(), R.layout.row, R.id.title, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      View row=super.getView(position, convertView, parent);
+      Item item=getItem(position);
+      ImageView icon=(ImageView)row.findViewById(R.id.icon);
+
+      Picasso.with(getActivity()).load(item.owner.profileImage)
+             .fit().centerCrop()
+             .placeholder(R.drawable.owner_placeholder)
+             .error(R.drawable.owner_error).into(icon);
+
+      TextView title=(TextView)row.findViewById(R.id.title);
+
+      title.setText(Html.fromHtml(getItem(position).title));
+
+      return(row);
+    }
+  }
+}

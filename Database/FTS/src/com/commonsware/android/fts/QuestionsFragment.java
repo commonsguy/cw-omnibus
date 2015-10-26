a7460aaf3ddd97492012b5e82e368c8509e86ccb
Mark Murphy <mmurphy@commonsware.com>
2015-10-26 08:05:06 -0400

    fixed copyright notice typo

--- Database/FTS/src/com/commonsware/android/fts/QuestionsFragment.java
+++ Database/FTS/src/com/commonsware/android/fts/QuestionsFragment.java
@@ -1,5 +1,5 @@
 /***
-  Copyright (c) 20132-14 CommonsWare, LLC
+  Copyright (c) 2013-2014 CommonsWare, LLC
   Licensed under the Apache License, Version 2.0 (the "License"); you may not
   use this file except in compliance with the License. You may obtain a copy
   of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required

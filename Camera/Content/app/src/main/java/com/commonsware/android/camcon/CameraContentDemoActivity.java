53b13d5502b7a6a39c3d1e1685651418755461e4
Mark Murphy <mmurphy@commonsware.com>
2016-03-20 16:39:17 -0400

    handle config changes/process termination

--- Camera/Content/app/src/main/java/com/commonsware/android/camcon/CameraContentDemoActivity.java
+++ Camera/Content/app/src/main/java/com/commonsware/android/camcon/CameraContentDemoActivity.java
@@ -1,3 +1,17 @@
+/***
+ Copyright (c) 2008-2016 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+
+ From _The Busy Coder's Guide to Android Development_
+ https://commonsware.com/Android
+ */
+
 package com.commonsware.android.camcon;
 
 import android.app.Activity;
@@ -9,6 +23,9 @@ import android.provider.MediaStore;
 import java.io.File;
 
 public class CameraContentDemoActivity extends Activity {
+  private static final String EXTRA_FILENAME=
+    "com.commonsware.android.camcon.EXTRA_FILENAME";
+  private static final String FILENAME="CameraContentDemo.jpeg";
   private static final int CONTENT_REQUEST=1337;
   private File output=null;
 
@@ -17,13 +34,32 @@ public class CameraContentDemoActivity extends Activity {
     super.onCreate(savedInstanceState);
 
     Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    File dir=
+
+    if (savedInstanceState==null) {
+      File dir=
         Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
 
-    output=new File(dir, "CameraContentDemo.jpeg");
+      dir.mkdirs();
+      output=new File(dir, FILENAME);
+    }
+    else {
+      output=(File)savedInstanceState.getSerializable(EXTRA_FILENAME);
+    }
+
+    if (output.exists()) {
+      output.delete();
+    }
+
     i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(output));
 
     startActivityForResult(i, CONTENT_REQUEST);
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+
+    outState.putSerializable(EXTRA_FILENAME, output);
   }
 
   @Override

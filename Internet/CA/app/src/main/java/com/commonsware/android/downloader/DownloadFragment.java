c38dc515644202a89c40ef9f7a218a620b69b8b5
Mark Murphy <mmurphy@commonsware.com>
2016-03-28 13:47:59 -0400

    new sample app, for Android N CA/certificate pinning

---
+++ Internet/CA/app/src/main/java/com/commonsware/android/downloader/DownloadFragment.java
@@ -0,0 +1,83 @@
+/***
+  Copyright (c) 2008-2014 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+ */
+
+package com.commonsware.android.downloader;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v13.app.FragmentCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class DownloadFragment extends Fragment implements
+    View.OnClickListener {
+  private static final String[] PERMS_ALL=
+    {WRITE_EXTERNAL_STORAGE};
+  private static final int REQUEST_ALL=123;
+  private Button b=null;
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                           Bundle savedInstanceState) {
+    View result=inflater.inflate(R.layout.main, parent, false);
+
+    b=(Button)result.findViewById(R.id.button);
+    b.setOnClickListener(this);
+
+    return(result);
+  }
+
+  @Override
+  public void onClick(View v) {
+    if (hasPermission(WRITE_EXTERNAL_STORAGE)) {
+      doTheDownload();
+    }
+    else {
+      FragmentCompat.requestPermissions(this, PERMS_ALL, REQUEST_ALL);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode,
+                                         String[] permissions,
+                                         int[] grantResults) {
+    if (requestCode==REQUEST_ALL) {
+      if (hasPermission(WRITE_EXTERNAL_STORAGE)) {
+        doTheDownload();
+      }
+    }
+  }
+
+  private void doTheDownload() {
+    Intent i=new Intent(getActivity(), Downloader.class);
+
+    i.setData(Uri.parse(
+      "https://wares.commonsware.com/excerpt-7p0.pdf"));
+
+    getActivity().startService(i);
+    getActivity().finish();
+  }
+
+  private boolean hasPermission(String perm) {
+    return(ContextCompat.checkSelfPermission(getActivity(), perm)==
+      PackageManager.PERMISSION_GRANTED);
+  }
+}
---
+++ Internet/CA/app/src/main/java/com/commonsware/android/downloader/Downloader.java
@@ -0,0 +1,150 @@
+/***
+ * Copyright (c) 2008-2012 CommonsWare, LLC
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not
+ * use this file except in compliance with the License. You may
+ * obtain	a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required
+ * by applicable law or agreed to in writing, software
+ * distributed under the
+ * License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES
+ * OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for
+ * the specific
+ * language governing permissions and limitations under the
+ * License.
+ * <p/>
+ * From _The Busy Coder's Guide to Android Development_
+ * https://commonsware.com/Android
+ */
+
+package com.commonsware.android.downloader;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Downloader extends IntentService {
+  private static final String AUTHORITY=
+    BuildConfig.APPLICATION_ID+".provider";
+  private static int NOTIFY_ID=1337;
+  private static int FOREGROUND_ID=1338;
+
+  public Downloader() {
+    super("Downloader");
+  }
+
+  @Override
+  public void onHandleIntent(Intent i) {
+    File output=new File(getFilesDir(),
+      i.getData().getLastPathSegment());
+
+    startForeground(FOREGROUND_ID,
+      buildForegroundNotification(output.getName()));
+
+    try {
+      if (output.exists()) {
+        output.delete();
+      }
+
+      URL url=new URL(i.getData().toString());
+      HttpURLConnection c=
+        (HttpURLConnection)url.openConnection();
+      FileOutputStream fos=
+        new FileOutputStream(output.getPath());
+      BufferedOutputStream out=new BufferedOutputStream(fos);
+      String mimeType=c.getHeaderField("Content-type");
+
+      try {
+        InputStream in=c.getInputStream();
+        byte[] buffer=new byte[8192];
+        int len=0;
+
+        while ((len=in.read(buffer))>=0) {
+          out.write(buffer, 0, len);
+        }
+
+        out.flush();
+      }
+      finally {
+        fos.getFD().sync();
+        out.close();
+        c.disconnect();
+      }
+
+      raiseNotification(mimeType, output, null);
+    }
+    catch (IOException e2) {
+      Log.e(getClass().getSimpleName(),
+        "Exception downloading file", e2);
+      raiseNotification(null, null, e2);
+    }
+    finally {
+      stopForeground(true);
+    }
+  }
+
+  private void raiseNotification(String mimeType, File output,
+                                 Exception e) {
+    NotificationCompat.Builder b=
+      new NotificationCompat.Builder(this);
+
+    b.setAutoCancel(true).setDefaults(Notification.DEFAULT_ALL);
+
+    if (e==null) {
+      b.setContentTitle(getString(R.string.download_complete))
+        .setContentText(getString(R.string.fun))
+        .setSmallIcon(android.R.drawable.stat_sys_download_done)
+        .setTicker(getString(R.string.download_complete));
+
+      Intent outbound=new Intent(Intent.ACTION_VIEW);
+      Uri doc=
+        FileProvider.getUriForFile(this, AUTHORITY, output);
+
+      outbound.setDataAndType(doc, mimeType);
+      outbound.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+      b.setContentIntent(
+        PendingIntent.getActivity(this, 0, outbound, 0));
+    }
+    else {
+      b.setContentTitle(getString(R.string.exception))
+        .setContentText(e.getMessage())
+        .setSmallIcon(android.R.drawable.stat_notify_error)
+        .setTicker(getString(R.string.exception));
+    }
+
+    NotificationManager mgr=
+      (NotificationManager)getSystemService(
+        NOTIFICATION_SERVICE);
+
+    mgr.notify(NOTIFY_ID, b.build());
+  }
+
+  private Notification buildForegroundNotification(String filename) {
+    NotificationCompat.Builder b=new NotificationCompat.Builder(this);
+
+    b.setOngoing(true);
+
+    b.setContentTitle(getString(R.string.downloading))
+      .setContentText(filename)
+      .setSmallIcon(android.R.drawable.stat_sys_download)
+      .setTicker(getString(R.string.downloading));
+
+    return(b.build());
+  }
+}

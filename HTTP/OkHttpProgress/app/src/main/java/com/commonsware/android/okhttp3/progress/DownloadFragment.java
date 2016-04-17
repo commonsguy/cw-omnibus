f4662752db48badce5c82ff468b01fca9e0a91c5
Mark Murphy <mmurphy@commonsware.com>
2016-04-17 14:00:12 -0400

    new book sample

---
+++ HTTP/OkHttpProgress/app/src/main/java/com/commonsware/android/okhttp3/progress/DownloadFragment.java
@@ -0,0 +1,51 @@
+/***
+  Copyright (c) 2008-2016 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+	
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+ */
+
+package com.commonsware.android.okhttp3.progress;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class DownloadFragment extends Fragment implements
+    View.OnClickListener {
+  private Button b=null;
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                           Bundle savedInstanceState) {
+    View result=inflater.inflate(R.layout.main, parent, false);
+
+    b=(Button)result.findViewById(R.id.button);
+    b.setOnClickListener(this);
+
+    return(result);
+  }
+
+  @Override
+  public void onClick(View v) {
+    Intent i=new Intent(getActivity(), Downloader.class);
+
+    i.setDataAndType(Uri.parse("http://commonsware.com/Android/Android-1_1-CC.pdf"),
+                     "application/pdf");
+
+    getActivity().startService(i);
+    getActivity().finish();
+  }
+}
---
+++ HTTP/OkHttpProgress/app/src/main/java/com/commonsware/android/okhttp3/progress/ResponseBodyWrapper.java
@@ -0,0 +1,53 @@
+/***
+ Copyright (c) 2008-2016 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain	a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+
+ From _The Busy Coder's Guide to Android Development_
+ https://commonsware.com/Android
+ */
+
+package com.commonsware.android.okhttp3.progress;
+
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
+import okio.BufferedSource;
+import okio.Okio;
+import okio.Source;
+
+// inspired by https://github.com/square/okhttp/blob/master/samples/guide/src/main/java/okhttp3/recipes/Progress.java
+
+abstract class ResponseBodyWrapper extends ResponseBody {
+  abstract Source wrapSource(Source original);
+
+  private final ResponseBody wrapped;
+  private BufferedSource buffer;
+
+  ResponseBodyWrapper(ResponseBody wrapped) {
+    this.wrapped=wrapped;
+  }
+
+  @Override
+  public MediaType contentType() {
+    return(wrapped.contentType());
+  }
+
+  @Override
+  public long contentLength() {
+    return(wrapped.contentLength());
+  }
+
+  @Override
+  public BufferedSource source() {
+    if (buffer==null) {
+      buffer=Okio.buffer(wrapSource(wrapped.source()));
+    }
+
+    return(buffer);
+  }
+}

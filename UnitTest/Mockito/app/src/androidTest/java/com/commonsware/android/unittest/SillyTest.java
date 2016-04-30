cbd54a3e60a6041b3d3408e19775ac7cdd7d85dd
Mark Murphy <mmurphy@commonsware.com>
2016-04-30 10:36:51 -0400

    formatting fixes

--- UnitTest/Mockito/app/src/androidTest/java/com/commonsware/android/unittest/SillyTest.java
+++ UnitTest/Mockito/app/src/androidTest/java/com/commonsware/android/unittest/SillyTest.java
@@ -1,15 +1,15 @@
 /***
-	Copyright (c) 2008-2016 CommonsWare, LLC
-	Licensed under the Apache License, Version 2.0 (the "License"); you may not
-	use this file except in compliance with the License. You may obtain a copy
-	of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
-	by applicable law or agreed to in writing, software distributed under the
-	License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-	OF ANY KIND, either express or implied. See the License for the specific
-	language governing permissions and limitations under the License.
-	
-	From _The Busy Coder's Guide to Android Development_
-		https://commonsware.com/Android
+    Copyright (c) 2008-2016 CommonsWare, LLC
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy
+    of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+    by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied. See the License for the specific
+    language governing permissions and limitations under the License.
+    
+    From _The Busy Coder's Guide to Android Development_
+        https://commonsware.com/Android
 */
 
 package com.commonsware.android.unittest;
@@ -32,13 +32,13 @@ public class SillyTest {
 
   @Before
   public void doThisFirst() {
-		// do initialization here, run on every test method
-	}
+        // do initialization here, run on every test method
+    }
 
   @After
   public void doThisLast() {
-		// do termination here, run on every test method
-	}
+        // do termination here, run on every test method
+    }
 
   @AfterClass
   static public void doThisLastOnlyOnce() {
@@ -46,7 +46,7 @@ public class SillyTest {
   }
 
   @Test
-	public void thisIsReallySilly() {
+    public void thisIsReallySilly() {
     Assert.assertEquals("bit got flipped by cosmic rays", 1, 1);
-	}
+    }
 }
--- UnitTest/Mockito/app/src/test/java/com/commonsware/android/unittest/SOTests.java
+++ UnitTest/Mockito/app/src/test/java/com/commonsware/android/unittest/SOTests.java
@@ -1,15 +1,15 @@
 /***
-	Copyright (c) 2016 CommonsWare, LLC
-	Licensed under the Apache License, Version 2.0 (the "License"); you may not
-	use this file except in compliance with the License. You may obtain a copy
-	of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
-	by applicable law or agreed to in writing, software distributed under the
-	License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-	OF ANY KIND, either express or implied. See the License for the specific
-	language governing permissions and limitations under the License.
-	
-	From _The Busy Coder's Guide to Android Development_
-		https://commonsware.com/Android
+    Copyright (c) 2016 CommonsWare, LLC
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy
+    of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+    by applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied. See the License for the specific
+    language governing permissions and limitations under the License.
+    
+    From _The Busy Coder's Guide to Android Development_
+        https://commonsware.com/Android
 */
 
 package com.commonsware.android.unittest;
@@ -67,7 +67,7 @@ public class SOTests {
       }
     }).when(mockSO).questions(eq("android"), any(Callback.class));
 
-		mockSO.questions("android", new Callback<SOQuestions>() {
+        mockSO.questions("android", new Callback<SOQuestions>() {
       @Override
       public void success(SOQuestions soQuestions,
                           Response response) {
@@ -81,7 +81,7 @@ public class SOTests {
       }
     });
 
-		responseLatch.await();
+        responseLatch.await();
 
     Assert.assertNotNull(questions);
     Assert.assertEquals(1, questions.items.size());

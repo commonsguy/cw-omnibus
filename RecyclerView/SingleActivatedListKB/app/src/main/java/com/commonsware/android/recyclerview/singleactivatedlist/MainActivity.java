7961cf537dfacc39874c3bc75b175f54e8bc44fa
Mark Murphy <mmurphy@commonsware.com>
2016-06-12 08:03:38 -0400

    new sample app

---
+++ RecyclerView/SingleActivatedListKB/app/src/main/java/com/commonsware/android/recyclerview/singleactivatedlist/MainActivity.java
@@ -0,0 +1,71 @@
+/***
+  Copyright (c) 2008-2015 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+*/
+
+package com.commonsware.android.recyclerview.singleactivatedlist;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MainActivity extends RecyclerViewActivity {
+  private static final String[] items={"lorem", "ipsum", "dolor",
+          "sit", "amet",
+          "consectetuer", "adipiscing", "elit", "morbi", "vel",
+          "ligula", "vitae", "arcu", "aliquet", "mollis",
+          "etiam", "vel", "erat", "placerat", "ante",
+          "porttitor", "sodales", "pellentesque", "augue", "purus"};
+  private ChoiceCapableAdapter<?> adapter=null;
+  
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+
+    setLayoutManager(new LinearLayoutManager(this));
+    adapter=new IconicAdapter(getRecyclerView());
+    setAdapter(adapter);
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle state) {
+    adapter.onSaveInstanceState(state);
+  }
+
+  @Override
+  public void onRestoreInstanceState(Bundle state) {
+    adapter.onRestoreInstanceState(state);
+  }
+  
+  class IconicAdapter extends ChoiceCapableAdapter<RowController> {
+    IconicAdapter(RecyclerView rv) {
+      super(rv, new SingleChoiceMode());
+    }
+
+    @Override
+    public RowController onCreateViewHolder(ViewGroup parent, int viewType) {
+      return(new RowController(this, getLayoutInflater()
+                                      .inflate(R.layout.row, parent, false)));
+    }
+
+    @Override
+    public void onBindViewHolder(RowController holder, int position) {
+      holder.bindModel(items[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+      return(items.length);
+    }
+  }
+}
---
+++ RecyclerView/SingleActivatedListKB/app/src/main/java/com/commonsware/android/recyclerview/singleactivatedlist/RowController.java
@@ -0,0 +1,70 @@
+/***
+ Copyright (c) 2015 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain	a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+
+ From _The Busy Coder's Guide to Android Development_
+ https://commonsware.com/Android
+ */
+
+package com.commonsware.android.recyclerview.singleactivatedlist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class RowController extends RecyclerView.ViewHolder
+    implements View.OnClickListener {
+  private ChoiceCapableAdapter adapter;
+  private TextView label=null;
+  private TextView size=null;
+  private ImageView icon=null;
+  private String template=null;
+  private View row=null;
+
+  RowController(ChoiceCapableAdapter adapter, View row) {
+    super(row);
+
+    this.row=row;
+    this.adapter=adapter;
+    label=(TextView)row.findViewById(R.id.label);
+    size=(TextView)row.findViewById(R.id.size);
+    icon=(ImageView)row.findViewById(R.id.icon);
+
+    template=size.getContext().getString(R.string.size_template);
+
+    row.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    boolean isCheckedNow=adapter.isChecked(getAdapterPosition());
+
+    adapter.onChecked(getAdapterPosition(), !isCheckedNow);
+    row.setActivated(!isCheckedNow);
+  }
+
+  void bindModel(String item) {
+    label.setText(item);
+    size.setText(String.format(template, item.length()));
+
+    if (item.length()>4) {
+      icon.setImageResource(R.drawable.delete);
+    }
+    else {
+      icon.setImageResource(R.drawable.ok);
+    }
+
+    setChecked(adapter.isChecked(getAdapterPosition()));
+  }
+
+  void setChecked(boolean isChecked) {
+    row.setActivated(isChecked);
+  }
+}

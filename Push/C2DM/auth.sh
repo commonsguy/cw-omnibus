dd4aef8757b3e5938fb684e048500e25f931cb1e
Mark Murphy <mmurphy@commonsware.com>
2016-10-09 10:09:26 -0400

    removed obsolete samples

--- Push/C2DM/auth.sh
+++
@@ -1,1 +0,0 @@
-curl https://www.google.com/accounts/ClientLogin -d Email=$1 -d "Passwd=$2" -d accountType=GOOGLE -d source=Google-cURL-Example -d service=ac2dm
--- Push/C2DM/post.sh
+++
@@ -1,1 +0,0 @@
-curl --header "Authorization: GoogleLogin auth=$1" "https://android.apis.google.com/c2dm/send" -d registration_id=$2 -d "data.payload=$3" -d collapse_key=something

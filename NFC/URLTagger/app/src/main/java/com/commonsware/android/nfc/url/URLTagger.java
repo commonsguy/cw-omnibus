32f6125fd2805d96656733478b4290912d8c0512
Mark Murphy <mmurphy@commonsware.com>
2017-02-12 10:01:53 -0500

    updated header comment

--- NFC/URLTagger/app/src/main/java/com/commonsware/android/nfc/url/URLTagger.java
+++ NFC/URLTagger/app/src/main/java/com/commonsware/android/nfc/url/URLTagger.java
@@ -10,7 +10,7 @@
   OF ANY KIND, either express or implied. See the License for the specific
   language governing permissions and limitations under the License.
   
-  From _The Busy Coder's Guide to Android Development_
+  Covered in detail in the book _The Busy Coder's Guide to Android Development_
     https://commonsware.com/Android
 */
 

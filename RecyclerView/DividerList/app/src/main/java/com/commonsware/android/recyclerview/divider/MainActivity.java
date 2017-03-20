ba2ec13d3d14df40db84148e6d6b88be607fa509
Mark Murphy <mmurphy@commonsware.com>
2017-03-20 10:30:28 -0400

    new sample of RecyclerView dividers

---
+++ RecyclerView/DividerList/app/src/main/java/com/commonsware/android/recyclerview/divider/MainActivity.java
@@ -0,0 +1,91 @@
+/***
+  Copyright (c) 2008-2015 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain	a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  Covered in detail in the book _The Busy Coder's Guide to Android Development_
+    https://commonsware.com/Android
+*/
+
+package com.commonsware.android.recyclerview.divider;
+
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends RecyclerViewActivity {
+  private static final String[] items={"lorem", "ipsum", "dolor",
+          "sit", "amet",
+          "consectetuer", "adipiscing", "elit", "morbi", "vel",
+          "ligula", "vitae", "arcu", "aliquet", "mollis",
+          "etiam", "vel", "erat", "placerat", "ante",
+          "porttitor", "sodales", "pellentesque", "augue", "purus"};
+  
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+
+    setLayoutManager(new LinearLayoutManager(this));
+
+    getRecyclerView()
+      .addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+    setAdapter(new IconicAdapter());
+  }
+  
+  class IconicAdapter extends RecyclerView.Adapter<RowHolder> {
+    @Override
+    public RowHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      return(new RowHolder(getLayoutInflater()
+                            .inflate(R.layout.row, parent, false)));
+    }
+
+    @Override
+    public void onBindViewHolder(RowHolder holder, int position) {
+      holder.bindModel(items[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+      return(items.length);
+    }
+  }
+
+  static class RowHolder extends RecyclerView.ViewHolder {
+    TextView label=null;
+    TextView size=null;
+    ImageView icon=null;
+    String template=null;
+
+    RowHolder(View row) {
+      super(row);
+
+      label=(TextView)row.findViewById(R.id.label);
+      size=(TextView)row.findViewById(R.id.size);
+      icon=(ImageView)row.findViewById(R.id.icon);
+
+      template=size.getContext().getString(R.string.size_template);
+    }
+
+    void bindModel(String item) {
+      label.setText(item);
+      size.setText(String.format(template, item.length()));
+
+      if (item.length()>4) {
+        icon.setImageResource(R.drawable.delete);
+      }
+      else {
+        icon.setImageResource(R.drawable.ok);
+      }
+    }
+  }
+}
---
+++ RecyclerView/DividerList/app/src/main/java/com/commonsware/android/recyclerview/divider/RecyclerViewActivity.java
@@ -0,0 +1,44 @@
+/***
+ Copyright (c) 2008-2015 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain	a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+
+ Covered in detail in the book _The Busy Coder's Guide to Android Development_
+ https://commonsware.com/Android
+ */
+
+package com.commonsware.android.recyclerview.divider;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+
+public class RecyclerViewActivity extends Activity {
+  private RecyclerView rv=null;
+
+  public void setAdapter(RecyclerView.Adapter adapter) {
+    getRecyclerView().setAdapter(adapter);
+  }
+
+  public RecyclerView.Adapter getAdapter() {
+    return(getRecyclerView().getAdapter());
+  }
+
+  public void setLayoutManager(RecyclerView.LayoutManager mgr) {
+    getRecyclerView().setLayoutManager(mgr);
+  }
+
+  public RecyclerView getRecyclerView() {
+    if (rv==null) {
+      rv=new RecyclerView(this);
+      rv.setHasFixedSize(true);
+      setContentView(rv);
+    }
+
+    return(rv);
+  }
+}

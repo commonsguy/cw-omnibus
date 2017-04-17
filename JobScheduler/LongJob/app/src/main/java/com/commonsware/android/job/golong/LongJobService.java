1a50928c46e8c8ceaec43449752a4d02bc72552c
Mark Murphy <mmurphy@commonsware.com>
2017-04-17 12:57:15 -0400

    experiment in JobService job duration

---
+++ JobScheduler/LongJob/app/src/main/java/com/commonsware/android/job/golong/LongJobService.java
@@ -0,0 +1,35 @@
+/***
+ Copyright (c) 2017 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+
+ Covered in detail in the book _The Busy Coder's Guide to Android Development_
+ https://commonsware.com/Android
+ */
+
+package com.commonsware.android.job.golong;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.util.Log;
+
+public class LongJobService extends JobService {
+  @Override
+  public boolean onStartJob(JobParameters params) {
+    Log.e(getClass().getSimpleName(), "onStartJob() called");
+
+    return(true);
+  }
+
+  @Override
+  public boolean onStopJob(JobParameters params) {
+    Log.e(getClass().getSimpleName(), "onStartJob() called");
+
+    return(false);
+  }
+}

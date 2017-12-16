795cc9b640b2b5d189181b335b1bd3561e5d2fb5
Mark Murphy <mmurphy@commonsware.com>
2017-12-16 07:29:45 -0500

    updated to 11.6.2, removed license terms stuff

--- MapsV2/FlatMarkers/app/src/main/java/com/commonsware/android/mapsv2/flatmarkers/AbstractMapActivity.java
+++ MapsV2/FlatMarkers/app/src/main/java/com/commonsware/android/mapsv2/flatmarkers/AbstractMapActivity.java
@@ -1,5 +1,5 @@
 /***
-  Copyright (c) 2012-2015 CommonsWare, LLC
+  Copyright (c) 2012-2017 CommonsWare, LLC
   Licensed under the Apache License, Version 2.0 (the "License"); you may not
   use this file except in compliance with the License. You may obtain a copy
   of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
@@ -19,36 +19,15 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.FeatureInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
-public class AbstractMapActivity extends Activity {
+abstract public class AbstractMapActivity extends Activity {
   static final String TAG_ERROR_DIALOG_FRAGMENT="errorDialog";
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.activity_main, menu);
-
-    return(super.onCreateOptionsMenu(menu));
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == R.id.legal) {
-      startActivity(new Intent(this, LegalNoticesActivity.class));
-
-      return(true);
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
 
   protected boolean readyToGo() {
     GoogleApiAvailability checker=
